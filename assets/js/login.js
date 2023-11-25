23315b8a21b47209027ab4b009dfdc451864f7cb
Sumit Raghav <sumit@moneywide.com>
2023-11-25 21:27:30 +0530

    changes

--- assets/js/login.js
+++ assets/js/login.js
@@ -23,7 +23,7 @@ var deviceId = 2;
                             $("#submit").val('Please Wait....');
                         },
                         success: function (response) {
-                            console.log(response);
+                            alert(response);
                             var data = $.parseJSON(response);
                             $("#err").addClass("red").text("");
                             if (data.status == 'error') {
