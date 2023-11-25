1788299e44fbdb9bea2d6147d36154b2ddd90cc1
Sumit Raghav <sumit@moneywide.com>
2023-11-25 21:36:13 +0530

    changes

--- config/session.php
+++ config/session.php
@@ -6,7 +6,7 @@ $show_external_api_data = 1;
 
 echo "hellooooo";
 if(!is_numeric($user_id) || $user_id == 0 || !in_array($user_role,array(1,2,3,4,5,6,7,9,8,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26))){
-  // header("location:".$head_url."/logout.php");
+  header("location:".$head_url."/logout.php");
     exit;
 }
 ?>
