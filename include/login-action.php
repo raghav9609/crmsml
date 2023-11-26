8ea755d3d0a822b2e28625e05dacd20f32101da8
Sumit Raghav <sumit@moneywide.com>
2023-11-26 11:27:45 +0530

    changes

--- include/login-action.php
+++ include/login-action.php
@@ -32,7 +32,7 @@ if(requestMethod() != 'POST'){
                                                 "role_id"=>$user_data[0]['role_id'],
                                                 "user_login_datetime"=>$user_data[0]['last_login_on']
                                             );
-                //print_r($_SESSION);
+                print_r($_SERVER);
                 $status = 'success';
                 $message = 'Details Fetch Successfully';
                 $goto = $head_url."/query/";               
