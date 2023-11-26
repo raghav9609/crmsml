53741f782f1b4e43ebd42cf933a3c88b26f08f3c
Sumit <sumit@moneywide.com>
2023-11-26 13:29:05 +0530

    as

--- query/index.php
+++ query/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 print_r($_SESSION);
+phpinfo();
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once(dirname(__FILE__) . '/../include/header.php');
 //exit;
