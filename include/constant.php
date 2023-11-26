64fc8104e2b0e0dc2b29db6fcf5d4e00c9ef0681
Sumit <sumit@moneywide.com>
2023-11-26 14:20:24 +0530

    as

--- include/constant.php
+++ include/constant.php
@@ -1,5 +1,4 @@
 <?php
-$no_head = 0;
 $multi_browser_array = array();
 $global_otp_flag = 1; //1 - Dynamic OTP, 2 - Static OTP , 3- For Auto Login
 $sms_api_vendor = 1;
@@ -11,19 +10,23 @@ $offset = 0;
 $start_limit = 0;
 $max_offset = 11;
 $display_count = 10;
-// if ($_REQUEST['page']!= "") {
-//     $page = $_REQUEST['page'];
-//     $offset = ($page - 1) * $display_count;
-// }
+if ($_REQUEST['page']!= "") {
+    $page = $_REQUEST['page'];
+    $offset = ($page - 1) * $display_count;
+}
 
 $page_no_onboarding = 1;
 $onboarding_offset = 0;
 $onboarding_max_offset = 110;
 $onboarding_display_count = 100;
-// if (isset($_REQUEST['page_no_onboarding'])) {
-//     $page_no_onboarding = replace_special($_REQUEST['page_no_onboarding']);
-//     $onboarding_offset = ($page_no_onboarding - 1) * $onboarding_display_count;
-// }
+if (isset($_REQUEST['page_no_onboarding'])) {
+    $page_no_onboarding = replace_special($_REQUEST['page_no_onboarding']);
+    $onboarding_offset = ($page_no_onboarding - 1) * $onboarding_display_count;
+}
 
- ?>
+if($no_head != 1){ ?>
+    <script>
+    var headURL = "<?php echo $head_url; ?>";
+</script>
+<?php } ?>
 
