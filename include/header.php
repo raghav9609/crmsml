2f65317f66b4cfa3117c5c7e6f73bc43fcacccbb
Sumit <sumit@moneywide.com>
2023-11-26 13:24:03 +0530

    as

--- include/header.php
+++ include/header.php
@@ -1,9 +1,7 @@
 <?php
 error_reporting(0); 
 require_once(dirname(__FILE__) . '/../config/config.php');
-//require_once(dirname(__FILE__) . '/../config/session.php');
-
-
+require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../model/headerModel.php');
 
 echo "sd";
