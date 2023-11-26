536fdb2941b4b69dd98c617cf3f06b0048361ee5
Sumit <sumit@moneywide.com>
2023-11-26 12:54:39 +0530

    s

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 //require_once(dirname(__FILE__) . '/../../../../config/config.php');
 require_once(dirname(__FILE__) . '/../include/constant.php');
 class DBController
