bdd85f552de895f31ad142a07bb91f19f0834818
Sumit Raghav <sumit@moneywide.com>
2023-11-26 12:56:41 +0530

    changes

--- include/header.php
+++ include/header.php
@@ -2,7 +2,10 @@
 error_reporting(0); 
 require_once(dirname(__FILE__) . '/../config/config.php');
 require_once(dirname(__FILE__) . '/../config/session.php');
-require_once(dirname(__FILE__) . '/../model/headerModel.php');
+exit;
+
+
+//require_once(dirname(__FILE__) . '/../model/headerModel.php');
 
 echo "sd";
 ?>
