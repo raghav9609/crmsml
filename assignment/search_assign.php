44d7eab25a75bfa32fdf7aa6d9e724df3401da0b
Sumit <sumit@moneywide.com>
2023-11-30 15:54:55 +0530

    as

--- assignment/search_assign.php
+++ assignment/search_assign.php
@@ -26,89 +26,81 @@ $query = mysqli_query($Conn1, $qry) or die(mysqli_error($Conn1));
                     <th>City Group</th>
                     <th>Salary Range</th>
                     <th>Loan Amount</th>
-                   
                     <th>Ist Shift</th>
                     <th>IInd Shift</th>
                 </tr>
                 <?php
-
-                $row_counter = mysqli_num_rows($query);
-                while ($result_query = mysqli_fetch_array($query)){
-                //$row_counter = mysqli_num_rows($result_query);
-                $id = $result_query['filter_id'];
-                $loan_type = $result_query['loan_type'];
-                $sub_group_name = $result_query['city_sub_group_name'];
-                $min_loan = $result_query['min_loan'];
-                $max_loan = $result_query['max_loan'];
-
-                $min_sal = $result_query['min_sal'];
-                $max_sal = $result_query['max_sal'];
+                    $row_counter = mysqli_num_rows($query);
+                    while ($result_query = mysqli_fetch_array($query)){
+                        $id = $result_query['filter_id'];
+                        $loan_type = $result_query['loan_type'];
+                        $sub_group_name = $result_query['city_sub_group_name'];
+                        $min_loan = $result_query['min_loan'];
+                        $max_loan = $result_query['max_loan'];
+                        $min_sal = $result_query['min_sal'];
+                        $max_sal = $result_query['max_sal'];
       
 
-                $get_loan_name_qry = mysqli_query($Conn1, "select value As loan_type_name from crm_masters where crm_masters_code_id = 1 and id = '" . $loan_type . "'");
-                $result_loan_name = mysqli_fetch_array($get_loan_name_qry);
-                $loan_name = $result_loan_name['loan_type_name'];
+                        $get_loan_name_qry = mysqli_query($Conn1, "select value As loan_type_name from crm_masters where crm_masters_code_id = 1 and id = '" . $loan_type . "'");
+                        $result_loan_name = mysqli_fetch_array($get_loan_name_qry);
+                        $loan_name = $result_loan_name['loan_type_name'];
 
-                if ($loan_type != '' && $loan_type > 0) {
-                    $get_qry_user = "select user.id as user_id,user.name as user_name from crm_master_user as user join crm_user_loan_type_mapping as map on user.id = map.user_id where user.is_active = 1 and map.loan_type = '" . $loan_type . "' group by user.id order by user.name";
-                } else {
-                    $get_qry_user = "select id As user_id,name As user_name from crm_master_user where is_active = 1";
-                }
-                $user_query_data = "select * from crm_lead_assignment where id = $id ";
-                $script[] = '<script type="text/javascript">
-$("#' . $id . '").change(function(){
-if (this.checked) {
-            $(".' . $id . '_chng").removeAttr("disabled");
-			$(".' . $id . '_chng").addClass("new_textbox");
-        } else { 
-			$(".' . $id . '_chng").attr("disabled", true);
-            $(".' . $id . '_chng").removeClass("new_textbox"); 
-            $("#selectAll").removeAttr("checked");
-    }
-});
-</script>';
-                ?>
+                        if ($loan_type != '' && $loan_type > 0) {
+                            $get_qry_user = "select user.id as user_id,user.name as user_name from crm_master_user as user join crm_user_loan_type_mapping as map on user.id = map.user_id where user.is_active = 1 and map.loan_type = '" . $loan_type . "' group by user.id order by user.name";
+                        } else {
+                            $get_qry_user = "select id As user_id,name As user_name from crm_master_user where is_active = 1";
+                        }
+                        $user_query_data = "select * from crm_lead_assignment where id = $id ";
+                        $script[] = '<script type="text/javascript">
+                            $("#' . $id . '").change(function(){
+                            if (this.checked) {
+                                $(".' . $id . '_chng").removeAttr("disabled");
+			                    $(".' . $id . '_chng").addClass("new_textbox");
+                            } else { 
+			                    $(".' . $id . '_chng").attr("disabled", true);
+                                $(".' . $id . '_chng").removeClass("new_textbox"); 
+                                $("#selectAll").removeAttr("checked");
+                            }
+                            });
+                        </script>';
+                    ?>
                 <tr>
-                    <td><input type="checkbox" name="ch_edit[]" value="<?php echo $id ?>" id="<?php echo $id ?>"
-                               class="loan_type abcd"/></td>
+                    <td><input type="checkbox" name="ch_edit[]" value="<?php echo $id ?>" id="<?php echo $id ?>" class="loan_type abcd"/></td>
 
-                     <td rowspan="<?php echo $row_counter ?>">
-                            <?php echo $sub_group_name . "<br><span class='orange'>" . $loan_name  ."</span><br>".$final_business_registered_loan_name."<br> Cross Sell " ;echo ($cross!='')?$cross:'-'; ?>
-                        </td>
-                    <td><input id="<?php echo $id ?>_amt" name="salary_<?php echo $id ?>"
-                               value="<?php echo $min_sal . " - " . $max_sal ?>" class="<?php echo $id; ?>_chng"
-                               disabled="" type="text"/>
+                    <td rowspan="<?php echo $row_counter ?>">
+                        <?php echo $sub_group_name . "<br><span class='orange'>" . $loan_name  ."</span><br>"; ?>
                     </td>
-                    <td><input type='hidden' name='loan_id' value='<?php echo $loan_type; ?>'/>
-                        <input id="<?php echo $id ?>_lamt" name="loan_amt_<?php echo $id ?>"
-                               value="<?php echo $min_loan . " - " . $max_loan ?>" class="<?php echo $id; ?>_chng"
-                               disabled="" type="text"/>
+                    <td>
+                        <input id="<?php echo $id ?>_amt" name="salary_<?php echo $id ?>" value="<?php echo $min_sal . " - " . $max_sal ?>" class="<?php echo $id; ?>_chng" disabled="" type="text"/>
+                    </td>
+                    <td>
+                        <input type='hidden' name='loan_id' value='<?php echo $loan_type; ?>'/>
+                        <input id="<?php echo $id ?>_lamt" name="loan_amt_<?php echo $id ?>" value="<?php echo $min_loan . " - " . $max_loan ?>" class="<?php echo $id; ?>_chng" disabled="" type="text"/>
                     </td>
                   
                     <td>
                         <?php
                         $shift_1_qry = mysqli_query($Conn1, $user_query_data . " and shift_id = 1");
                         while ($result_user_query = mysqli_fetch_assoc($shift_1_qry)) {
-                            $datacheck = $result_user_query['shift_flag'];
+                            $datacheck = $result_user_query['shift_id'];
                             $user_id_first = $result_user_query['user_id'];
-                            $assign_id = $result_user_query['assign_id'];
+                            //$assign_id = $result_user_query['assign_id'];
                             if ($datacheck == 1) {
                                 $user_query = mysqli_query($Conn1, $get_qry_user);
                                 ?>
                                 <select id="<?php echo $id ?>_name" disabled="" name="user_id_<?php echo $id ?>[]"
                                         class="<?php echo $id ?>_test <?php echo $id; ?>_chng">
-                                    <option value="0,<?php echo $assign_id; ?>">Select User</option>
+                                    <option value="0,<?php // echo $assign_id; ?>">Select User</option>
                                     <?php
                                     while ($result_user_query = mysqli_fetch_array($user_query)) {
                                         $sml_user_id = $result_user_query['user_id'];
                                         $sml_user_name = $result_user_query['user_name']; ?>
-                                        <option value="<?php echo $sml_user_id . ',' . $assign_id; ?>" <?php if ($sml_user_id == $user_id_first) { ?> selected <?php } ?>><?php echo $sml_user_name; ?></option>
+                                        <option value="<?php echo $sml_user_id; ?>" <?php if ($sml_user_id == $user_id_first) { ?> selected <?php } ?>><?php echo $sml_user_name; ?></option>
                                     <?php } ?>
                                 </select>&nbsp;&nbsp;
                                 <?php
                             }
-                        }
-                        ?>
+                        } ?>
                     </td>
                     <td>
                         <?php
@@ -116,7 +108,7 @@ if (this.checked) {
                         while ($result_user_query = mysqli_fetch_assoc($shift_2_qry)) {
                             $datacheck = $result_user_query['shift_flag'];
                             $user_id_first = $result_user_query['user_id'];
-                            $assign_id = $result_user_query['assign_id'];
+                            //$assign_id = $result_user_query['assign_id'];
 
                             $i = 1;
                             if ($datacheck == 2) {
@@ -125,12 +117,12 @@ if (this.checked) {
                                 ?>
                                 <select id="<?php echo $id ?>_name1" disabled="" name="user_id1_<?php echo $id ?>[]"
                                         class="<?php echo $id ?>_test1 <?php echo $id; ?>_chng">
-                                    <option value="0,<?php echo $assign_id; ?>">Select User</option>
+                                    <option value="0">Select User</option>
                                     <?php
                                     while ($result_user_query = mysqli_fetch_array($user_query)) {
                                         $sml_user_id = $result_user_query['user_id'];
                                         $sml_user_name = $result_user_query['user_name']; ?>
-                                        <option value="<?php echo $sml_user_id . ',' . $assign_id; ?>" <?php if ($sml_user_id == $user_id_first) { ?> selected <?php } ?>><?php echo $sml_user_name; ?></option>
+                                        <option value="<?php echo $sml_user_id; ?>" <?php if ($sml_user_id == $user_id_first) { ?> selected <?php } ?>><?php echo $sml_user_name; ?></option>
                                     <?php } ?>
                                 </select>&nbsp;&nbsp;
                                 <?php
@@ -139,7 +131,7 @@ if (this.checked) {
                         }
                         ?>
                     </td>
-                    </tr>
+                </tr>
                     <?php } ?>
             </table>
         </form>
