ad86412df291d7861b010580b6e55e3242955f81
Sumit <sumit@moneywide.com>
2023-11-30 15:12:01 +0530

    as

--- assignment/user_assign.php
+++ assignment/user_assign.php
@@ -1,5 +1,6 @@
 <?php 
-require_once "../../include/config.php";
+require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
+require_once(dirname(__FILE__) . '/../config/config.php');
 $loan_type = $_REQUEST['loan_type'];
 $type = replace_special($_REQUEST['type']);
 if($type === 'user_ist'){
@@ -10,7 +11,7 @@ if($type === 'user_ist'){
 ?>
 <select name="<?php echo $name;?>[]"><option value="">Select User Name</option>
 <?php 
-$qry_user = mysqli_query($Conn1,"select u_assign.user_id, u_assign.user_name from tbl_user_assign as u_assign left JOIN tl_loan_type_assign as l_assign ON u_assign.user_id = l_assign.tl_id  where l_assign.loan_type='".replace_special($_REQUEST['loan_type'])."'");
+$qry_user = mysqli_query($Conn1,"select u_assign.id As user_id, u_assign.name As user_name from crm_master_user as u_assign left JOIN crm_user_loan_type_mapping as l_assign ON u_assign.id = l_assign.user_id  where l_assign.loan_type='".replace_special($_REQUEST['loan_type'])."'");
 while($res_user = mysqli_fetch_array($qry_user)){?>
   <option value="<?php echo $res_user['user_id'];?>"><?php echo $res_user['user_name'];?></option>
   <?php } ?>
