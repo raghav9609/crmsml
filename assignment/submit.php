dfff016cf7d35e402401729f3a447c911d6f2674
Sumit <sumit@moneywide.com>
2023-11-30 15:21:30 +0530

    as

--- assignment/submit.php
+++ assignment/submit.php
@@ -6,6 +6,8 @@ $avail_first = replace_special($_REQUEST['user_ist_flag']);
 $user_secnd = replace_special($_REQUEST['user_2nd']);
 $avail_secnd = replace_special($_REQUEST['user_2nd_flag']);
 
+print_r($user_first);
+
 $user_id = 1;
 // echo "select * from crm_lead_assignment where loan_type ='" . replace_special($_REQUEST['loan_type']) . "' and min_loan_amount = '" . replace_special($_REQUEST['loan_frm']) . "' and max_loan_amount ='" . replace_special($_REQUEST['loan_to']) . "' and min_net_income ='" . replace_special($_REQUEST['salry_from']) . "' and max_net_income ='" . replace_special($_REQUEST['salry_to']) . "' and city_sub_group_id ='" . replace_special($_REQUEST['city_sub_group']) . "' and user_id = '".$user_id."'";
 
