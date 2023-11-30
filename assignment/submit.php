7d7a7f56ca60180a43ed31c138f207bf53bcd739
Sumit <sumit@moneywide.com>
2023-11-30 16:14:43 +0530

    as

--- assignment/submit.php
+++ assignment/submit.php
@@ -15,7 +15,7 @@ $arr = array('user_ist' => $user_first, 'user_2nd' => $user_secnd);
 
 foreach ($arr as $user => $val) {
     echo $user;
-    
+
     //$avail_flag = replace_special($_REQUEST[$user . "_flag"]);
     if ($user == 'user_ist') {
         $shift = 1;
@@ -23,6 +23,9 @@ foreach ($arr as $user => $val) {
         $shift = '2';
     }
     $count = count($val);
+    if ($count > 0) {
+
+    
     for ($i = 0; $i < $count; $i++) {
         $user_id = $val[$i];
         echo "select * from crm_lead_assignment where loan_type ='" . replace_special($_REQUEST['loan_type']) . "' and min_loan_amount = '" . replace_special($_REQUEST['loan_frm']) . "' and max_loan_amount ='" . replace_special($_REQUEST['loan_to']) . "' and min_net_income ='" . replace_special($_REQUEST['salry_from']) . "' and max_net_income ='" . replace_special($_REQUEST['salry_to']) . "' and city_sub_group_id ='" . replace_special($_REQUEST['city_sub_group']) . "' and user_id = '".$user_id."',shift_id ='" . replace_special($shift) . "'";
@@ -42,7 +45,7 @@ foreach ($arr as $user => $val) {
         
         }
     }
-    
+}
 } 
 
 header("location:index.php?msg=1");
