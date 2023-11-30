d2d541ea26d49a235d8cd9b16c887c39c59580fe
Sumit <sumit@moneywide.com>
2023-11-30 16:18:15 +0530

    as

--- assignment/submit.php
+++ assignment/submit.php
@@ -13,6 +13,7 @@ $user_id = 1;
 
 print_r($user_first);
 print_r($user_secnd);
+
 $arr = array('user_ist' => $user_first, 'user_2nd' => $user_secnd);
 
 foreach ($arr as $user => $val) {
@@ -25,7 +26,7 @@ foreach ($arr as $user => $val) {
         $shift = '2';
     }
     $count = count($val);
-    if ($count > 0) {
+    if (!empty($val)) {
         for ($i = 0; $i < $count; $i++) {
             $user_id = $val[$i];
             echo "select * from crm_lead_assignment where loan_type ='" . replace_special($_REQUEST['loan_type']) . "' and min_loan_amount = '" . replace_special($_REQUEST['loan_frm']) . "' and max_loan_amount ='" . replace_special($_REQUEST['loan_to']) . "' and min_net_income ='" . replace_special($_REQUEST['salry_from']) . "' and max_net_income ='" . replace_special($_REQUEST['salry_to']) . "' and city_sub_group_id ='" . replace_special($_REQUEST['city_sub_group']) . "' and user_id = '".$user_id."',shift_id ='" . replace_special($shift) . "'";
