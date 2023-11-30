d7ea9dd6345d855824028fdf794b886990027654
Sumit <sumit@moneywide.com>
2023-11-30 11:33:38 +0530

    as

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,7 +17,7 @@ var deviceId = 2;
                     $.ajax({
                         type: "POST",
                         data: "id=" + user + "&otp=" + pwd + "&deviceId=" + deviceId,
-                        url: headURL+"include/login-action.php",
+                        url: "https://astechnos.com/crmsml/include/login-action.php",
                         cache: false,
                         beforeSend: function () {
                             $("#submit").val('Please Wait....');
