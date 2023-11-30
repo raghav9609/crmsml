6ea754089b5cdb728002e7eccf004874ef4d9b68
Sumit <sumit@moneywide.com>
2023-11-30 13:51:45 +0530

    as

--- model/assignmentModel.php
+++ model/assignmentModel.php
@@ -12,15 +12,19 @@ class leadAssignMent{
             $query_to_execute .= " and user_id = ".$data['user_id'];
         }
         $query_to_execute .= " order by city_sub_group_id";
+        return $query_to_execute;
     }
     function leadAssignment($data){
         $query_to_execute = "select * from crm_lead_assignment where city_sub_group_id = ".$data['city_sub_group_id']." and min_net_income = ".$data['min_net_income']." and max_net_income = ".$data['max_net_income']." and min_loan_amount= ".$data['min_loan_amount']." and max_loan_amount =".$data['max_loan_amount']." and user_id = ".$data['user_id']." and is_active = ".$data['is_active']." and shift_id = ".$data['shift_id']." order by last_lead_assign_on DESC LIMIT 1";
+        return $query_to_execute;
     }
     function updateLeadAssignment($data){
         $query_to_execute = "update crm_lead_assignment set min_net_income = ".$data['min_net_income'].",max_net_income = ".$data['max_net_income'].", min_loan_amount= ".$data['min_loan_amount']." , max_loan_amount =".$data['max_loan_amount']." , user_id = ".$data['user_id']." where id =".$data['id'];
+        return $query_to_execute;
     }
     function updateLastLeadAssignDate($data){
         $query_to_execute = "update crm_lead_assignment set last_lead_assign_on = NOW() where id =".$data['id'];
+        return $query_to_execute;
     }
 }
 
