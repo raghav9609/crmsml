b8354e1114c228709be0338324ba666e2dfb4c59
Sumit <sumit@moneywide.com>
2023-11-30 14:58:19 +0530

    as

--- assignment/submit.php
+++ assignment/submit.php
@@ -6,6 +6,8 @@ $avail_first = replace_special($_REQUEST['user_ist_flag']);
 $user_secnd = replace_special($_REQUEST['user_2nd']);
 $avail_secnd = replace_special($_REQUEST['user_2nd_flag']);
 
+echo "select * from crm_lead_assignment where loan_type ='" . replace_special($_REQUEST['loan_type']) . "' and min_loan_amt = '" . replace_special($_REQUEST['loan_frm']) . "' and max_loan_amt ='" . replace_special($_REQUEST['loan_to']) . "' and min_net_income ='" . replace_special($_REQUEST['salry_from']) . "' and max_net_income ='" . replace_special($_REQUEST['salry_to']) . "' and city_sub_group_id ='" . replace_special($_REQUEST['city_sub_group']) . "'";
+
 $qry_search = mysqli_query($Conn1, "select * from crm_lead_assignment where loan_type ='" . replace_special($_REQUEST['loan_type']) . "' and min_loan_amt = '" . replace_special($_REQUEST['loan_frm']) . "' and max_loan_amt ='" . replace_special($_REQUEST['loan_to']) . "' and min_net_income ='" . replace_special($_REQUEST['salry_from']) . "' and max_net_income ='" . replace_special($_REQUEST['salry_to']) . "' and city_sub_group_id ='" . replace_special($_REQUEST['city_sub_group']) . "'");
 $res_search = mysqli_num_rows($qry_search);
 if ($res_search == '0') {
