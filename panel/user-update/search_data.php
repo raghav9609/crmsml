65f2121dd6b1d1bb17a58b2b0104f51abf18399c
Sumit <sumit@moneywide.com>
2023-12-01 16:39:00 +0530

    as

--- panel/user-update/search_data.php
+++ panel/user-update/search_data.php
@@ -9,36 +9,25 @@ require_once "../../config/config.php";
 <table class="gridtable" style='margin-left:2%;width:90%;'>
 <tr><th>User ID</th><th>User Name</th><th>Email</th><th>Cell number / Dialer number</th><th>Lead View / Count</th><th>Status</th><th>Hot Lead Limit</th><th>SME Flag</th><th>Last Login</th><th>Action</th></tr>
 <?php 
-	 $qry_info = "select u_ass.user_id as user_id,u_ass.user_name as user_name, u_ass.email as email,u_ass.contact_no as contact_no, u_ass.scontact_no as scontact_no, u_ass.one_lead_flag as one_lead_flag, u_ass.user_login_datetime as user_login_datetime, u_ass.status as status, u_ass.total_lead_limit as total_lead_limit, u_ass.open_lead_limit as open_lead_limit, tbl_user_role.role_id, tbl_user_role.role_type, u_ass.hot_lead_limit as hot_lead_limit, u_ass.sme_flag as sme_flag from tbl_user_assign as u_ass left join tbl_user_role on tbl_user_role.role_id = u_ass.role_id left join tl_loan_type_assign on tl_loan_type_assign.tl_id = u_ass.user_id where u_ass.user_id != ''";	 
+	 $qry_info = "select u_ass.id as user_id,u_ass.name as user_name, u_ass.email_id as email, u_ass.mobile_no as contact_no, u_ass.last_login_on as user_login_datetime, u_ass.is_active as status, urole.id as role_id, urole.role_name as role_type from crm_master_user as u_ass left join crm_master_user_role As urole on urole.id = u_ass.role_id left join crm_user_loan_type_mapping As ultyass on ultyass.user_id = u_ass.id where u_ass.id != ''";	 
    	 if($user_id != ''){
-   	 	$qry_info .= " and u_ass.user_name LIKE '".$user_id."%'";
+   	 	$qry_info .= " and u_ass.name LIKE '".$user_id."%'";
    	 }
     if($_REQUEST['status'] != ''){
-    	$qry_info .= " and u_ass.status = '".$_REQUEST['status']."'";
+    	$qry_info .= " and u_ass.is_active = '".$_REQUEST['status']."'";
     }
-    if($_REQUEST['lead_view'] != '') {
-      $qry_info .= " and u_ass.one_lead_flag = '".$_REQUEST['lead_view']."'";
-    }
-    if($_REQUEST['support_desk_flag'] != '') {
-      $qry_info .= " and u_ass.support_desk_flag = '".$_REQUEST['support_desk_flag']."'";
+
+    if(trim($_REQUEST['mobile_number']) != "") {
+      $qry_info .= " and u_ass.mobile_no = '".$_REQUEST['mobile_number']."' ";
     }
     
-    if($_REQUEST['mobile_recording_flag'] != "") {
-      $qry_info .= " and u_ass.mobile_recording_flag = '".$_REQUEST['mobile_recording_flag']."' ";
-    }
-    if(trim($_REQUEST['mobile_number']) != "") {
-      $qry_info .= " and u_ass.contact_no = '".$_REQUEST['mobile_number']."' ";
-    }
-    if(trim($_REQUEST['dialer_number']) != "") {
-      $qry_info .= " and u_ass.scontact_no = '".$_REQUEST['dialer_number']."' ";
-    }
     if(trim($_REQUEST['loan_type']) != "") {
-      $qry_info .= " and tl_loan_type_assign.loan_type = '".$_REQUEST['loan_type']."' ";
+      $qry_info .= " and crm_user_loan_type_mapping.loan_type = '".$_REQUEST['loan_type']."' ";
     }
     if(trim($_REQUEST['last_login']) != "" && trim($_REQUEST['last_login']) != "1970-01-01" && trim($_REQUEST['last_login']) != "0000-00-00") {
-      $qry_info .= " and date(u_ass.user_login_datetime) = '".$_REQUEST['last_login']."' ";
+      $qry_info .= " and date(u_ass.last_login_on) = '".$_REQUEST['last_login']."' ";
     }
-    $qry_info .= "group by u_ass.user_id";
+    $qry_info .= " group by u_ass.id";
 
     $result_info = mysqli_query($Conn1,$qry_info);
 	while($res_info = mysqli_fetch_array($result_info)){
@@ -53,10 +42,9 @@ require_once "../../config/config.php";
 <td><?php echo $res_info['user_name'];?></td>
 <td><?php echo $res_info['email'];?></td>
 <?php
-$scontact_no = ($res_info['scontact_no'] != "" && $res_info['scontact_no'] > 0) ? $res_info['scontact_no'] : "--";
+
 $contact_number = ($res_info['contact_no'] != "" && $res_info['contact_no'] > 0) ? $res_info['contact_no'] : "--";
-$open_count = ($res_info['open_lead_limit'] != "" && $res_info['open_lead_limit'] > 0) ? $res_info['open_lead_limit'] : "--";
-$total_count = ($res_info['total_lead_limit'] != "" && $res_info['total_lead_limit'] > 0) ? $res_info['total_lead_limit'] : "--";
+
 ?>
 <td><?php echo $contact_number."<br>(Dialer : ".$scontact_no.")";?></td>
 <td><?php echo ($res_info['one_lead_flag'] == 1 ? "One Lead" : "Grid View")."<br>";
