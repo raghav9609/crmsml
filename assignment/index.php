5d7b7ced26ef1b40044404514af80d3937690ea9
Sumit Raghav <sumit@moneywide.com>
2023-12-01 15:31:19 +0530

    changes

--- assignment/index.php
+++ assignment/index.php
@@ -41,70 +41,7 @@ require_once(dirname(__FILE__) . '/../include/helper.functions.php');
 <?php
 if (isset($_REQUEST["update"])) {
     $chech_id = replace_special($_REQUEST['ch_edit']);
-    for ($i = 0; $i < count($chech_id); $i++) {
-        $sal_ra = "salary_" . $chech_id[$i];
-        $loan_ra = "loan_amt_" . $chech_id[$i];
-        $user_id_prime = "user_id_" . $chech_id[$i];
-        $user_id_second = "user_id1_" . $chech_id[$i];
-        $flag_ac = "active_" . $chech_id[$i];
-        $occupation = "occupation_" . $chech_id[$i];
-        $hdfc = "hdfc_" . $chech_id[$i];
-//$employer_type = "employer_type_".$chech_id[$i];
-
-        $occupation_val = replace_special($_REQUEST[$occupation]);
-        $sal_range = replace_special($_REQUEST[$sal_ra]);
-        $loan_range_up = replace_special($_REQUEST[$loan_ra]);
-//$employer = replace_special($_REQUEST[$employer_type]);
-        $user_prime = $_REQUEST[$user_id_prime];
-        $user_second = $_REQUEST[$user_id_second];
-        $active_flag_up = replace_special($_REQUEST[$flag_ac]);
-        $hdfc_auto = replace_special($_REQUEST[$hdfc]);
-
-        $hdfc_cat_flag_up = implode(",", $_REQUEST["hdfc_cat_" . $chech_id[$i]]);
-        $salary_range_up = explode("-", $sal_range);
-        $loan_range_up = explode("-", $loan_range_up);
-        $city_sub_grp_id = replace_special($_REQUEST[$city_id]);
-        $loan_type_id = replace_special($_REQUEST[$loan_id]);
-        $loan_id = replace_special($_REQUEST['loan_id']);
-        $annutal_itr = "";
-        if($loan_id == 56 && $hdfc_cat_flag_up == ''){
-            $annutal_itr = "company_cat =0,";
-        }else if ($loan_id == 57) {
-            $itr_range = "itr_amt_" . $chech_id[$i];
-            $itr_up = replace_special($_REQUEST[$itr_range]);
-            $itr_up = explode("-", $itr_up);
-            $annutal_itr = "min_itr_amt = '" . $itr_up[0] . "' , max_itr_amt = '" . $itr_up[1] . "',";
-        }else if($loan_id == 56){
-            $annutal_itr = "company_cat = '" . $hdfc_cat_flag_up. "' ,";
-        }else if(in_array($loan_id,array(51,52,54))){
-            $annutal_itr = "hdfc_auto_push='" . $hdfc_auto . "',";
-        }
-
-        foreach ($user_prime as $data_prime) {
-
-            $user_id = explode(',', $data_prime);
-            $user_p = $user_id[0];
-            $assign_p = $user_id[1];
-            $query_p = "update tbl_assign_user_query_filter set user_id = '" . $user_p . "' where assign_id =  '" . $assign_p . "'";
-            $data_pri = mysqli_query($Conn1, $query_p);
-        }
-
-        foreach ($user_second as $data_second) {
-
-            $user_id = explode(',', $data_second);
-            $user_s = $user_id[0];
-            $assign_s = $user_id[1];
-            $query_s = "update tbl_assign_user_query_filter set user_id = '" . $user_s . "' where assign_id =  '" . $assign_s . "'";
-            $data_second = mysqli_query($Conn1, $query_s);
-
-            $query_s_history = mysqli_query($Conn1,"insert into tbl_assign_user_query_filter_history set user_id = '" . $user_s . "',assign_id =  '" . $assign_s . "',updated_by_user='".$user."',date=NOW()");
-        }
-
-
-        $query = "update crm_lead_assignment SET min_loan_amt = '" . $loan_range_up[0] . "', max_loan_amt= '" . $loan_range_up[1] . "', min_net_income = '" . $salary_range_up[0] . "', max_net_income = '" . $salary_range_up[1] . "' WHERE id = '" . $chech_id[$i] . "'";
-        $update_auto_query = mysqli_query($Conn1, $query);
-
-    }
+    print_r($_REQUEST);
 }
 ?>
 </body>
