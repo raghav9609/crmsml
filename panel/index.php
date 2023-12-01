25408e7ebceeac7180bce9f89b9b766ba1c73e9c
Sumit <sumit@moneywide.com>
2023-12-01 15:15:16 +0530

    as

--- panel/index.php
+++ panel/index.php
@@ -1,4 +1,6 @@
 <?php 
+require_once(dirname(__FILE__) . '/../config/session.php');
+require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once "../include/header.php";
 header("location:".$head_url."/panel/user-update/");
 ?>
