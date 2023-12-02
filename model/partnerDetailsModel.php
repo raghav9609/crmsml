9f481f7d965ec6e18d87d963cecc21a98aa30c65
Sumit Raghav <sumit@moneywide.com>
2023-12-02 15:33:30 +0530

    chages

--- model/partnerDetailsModel.php
+++ model/partnerDetailsModel.php
@@ -1,7 +1,7 @@
 <?php 
 class partnerDetails{
     function searchFilter($data){
-        $query_to_execute = "select * from crm_partner_rm_sm_details As partner where 1";
+        $query_to_execute = "select * from crm_partner_rm_sm_details where 1";
         if($data['city_id'] > 0){
             $query_to_execute .= " and city_id = ".$data['city_id'];
         }
@@ -21,6 +21,26 @@ class partnerDetails{
         $query_to_execute .= " order by partner_id";
         return $query_to_execute;
     }
+    // function updateDetails($da){
+    //     $query_to_execute = "update crm_partner_rm_sm_details set 1";
+    //     if($data['city_id'] > 0){
+    //         $query_to_execute .= " and city_id = ".$data['city_id'];
+    //     }
+    //     if($data['partner_id'] > 0){
+    //         $query_to_execute .= " and partner_id = ".$data['partner_id'];
+    //     }if($data['agent_type'] > 0){
+    //         $query_to_execute .= " and agent_type = ".$data['agent_type'];
+    //     }
+    //     if($data['sm_name'] != ""){
+    //         $query_to_execute .= " and name = '".$data['sm_name']."'";
+    //     }
+    //     if($data['sm_email_id'] != ""){
+    //         $query_to_execute .= " and email_id = '".$data['sm_email_id']."'";
+    //     }if($data['phoneno'] > 0){
+    //         $query_to_execute .= " and mobile_no = '".$data['phoneno']."'";
+    //     }
+    //     $query_to_execute .= " order by partner_id";
+    // }
 }
 
 $partnerDetailsExport = new partnerDetails();
