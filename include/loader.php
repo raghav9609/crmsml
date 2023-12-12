0f90f17cdf3cee6ae6656950b4514a8a3454e7a3
Sumit <sumit@moneywide.com>
2023-12-12 12:22:58 +0530

    as

--- include/loader.php
+++ include/loader.php
@@ -1,6 +1,6 @@
 <div id="loader" class="lds-css"> 
     <div class="lds-spinner">
-        <img src="<?php echo $head_url; ?>/images/loader-image.gif" alt="Loader" title="Loader">
+        <img src="<?php echo $head_url; ?>/assets/images/loader-image.gif" alt="Loader" title="Loader">
     </div>
     <b class="loader_text" style="letter-spacing:0.5px" id="loader_txt">
         Please wait......
