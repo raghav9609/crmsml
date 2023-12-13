e1cb13bef5679fbbde4424694d0c33824f05ff6a
aakash <aakash@moneywide.com>
2023-12-13 12:46:16 +0530

    changes

--- logout.php
+++ logout.php
@@ -10,15 +10,15 @@
     // print_r($_SESSION);
     // echo $head_url;
     // exit();
-    if (!empty($_SESSION['userDetails']['user_id'])){
-        $insert_arr = array(
-            "mlc_master_user_id"=>(int)$_SESSION['userDetails']['user_id'],
-            "ip"=>$login_ip,
-            "login_type"=>2
-        );
-        $ins_qry = $query_model->insertQueryData('sml_user_login_history',$insert_arr);
-        $db_handle->insertRows($ins_qry);
-    }
+    // if (!empty($_SESSION['userDetails']['user_id'])){
+    //     $insert_arr = array(
+    //         "mlc_master_user_id"=>(int)$_SESSION['userDetails']['user_id'],
+    //         "ip"=>$login_ip,
+    //         "login_type"=>2
+    //     );
+    //     $ins_qry = $query_model->insertQueryData('sml_user_login_history',$insert_arr);
+    //     $db_handle->insertRows($ins_qry);
+    // }
     session_destroy();
     header("location:".$head_url);
     
