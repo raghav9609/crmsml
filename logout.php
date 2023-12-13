5d4145e3e036c464ef1550ca918eb2c2515810f8
aakash <aakash@moneywide.com>
2023-12-13 12:53:01 +0530

    changes

--- logout.php
+++ logout.php
@@ -4,22 +4,24 @@
     require_once(dirname(__FILE__) . '/include/constant.php');
     require_once(dirname(__FILE__) . '/helpers/common-helper.php');
     require_once(dirname(__FILE__) . '/config/config.php');	
+    ob_start();
     session_start();
     $login_ip = ipAddress();
     // echo $_SESSION['userDetails']['user_id'];
     // print_r($_SESSION);
     // echo $head_url;
     // exit();
-    // if (!empty($_SESSION['userDetails']['user_id'])){
-    //     $insert_arr = array(
-    //         "mlc_master_user_id"=>(int)$_SESSION['userDetails']['user_id'],
-    //         "ip"=>$login_ip,
-    //         "login_type"=>2
-    //     );
-    //     $ins_qry = $query_model->insertQueryData('sml_user_login_history',$insert_arr);
-    //     $db_handle->insertRows($ins_qry);
-    // }
+    if (!empty($_SESSION['userDetails']['user_id'])){
+        $insert_arr = array(
+            "mlc_master_user_id"=>(int)$_SESSION['userDetails']['user_id'],
+            "ip"=>$login_ip,
+            "login_type"=>2
+        );
+        $ins_qry = $query_model->insertQueryData('sml_user_login_history',$insert_arr);
+        $db_handle->insertRows($ins_qry);
+    }
     session_destroy();
     header("location:".$head_url);
-    
+    ob_end_flush();
+    exit;
 ?>
