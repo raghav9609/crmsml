88545d2b2dc7f8f39f33d51e2fbb333c75c15342
aakash <aakash@moneywide.com>
2023-12-14 15:46:03 +0530

    changes

--- assignment/add-partner.php
+++ assignment/add-partner.php
@@ -11,7 +11,7 @@ require_once "../include/helper.functions.php";
 <tr><th>Loan Type: </th><td><?php echo get_dropdown(1,'loan_type',"","class='loan_type'");?></td></tr>
 <tr><th>Salary Range</td><td><input type="tel" name="salry_from" >  <input type="tel" name="salry_to" ></td></tr>
 <tr><th>Loan Amount</th><td><input type="tel" name="loan_frm" >  <input type="tel" name="loan_to" ></td></tr>
-<tr><th>ITR Amount</th><td><input type="tel" name="itr_frm" >  <input type="tel" name="itr_to" ></td></tr>
+<!-- <tr><th>ITR Amount</th><td><input type="tel" name="itr_frm" >  <input type="tel" name="itr_to" ></td></tr> -->
 <tr><th>Occupation</th><td><?php echo get_dropdown(7,'occupation',"","required");?></td></tr>
 
 <tr><th colspan="2"><input type="button" class=""  name="use_frst" data-id = "1" id="use_frst" style="margin-left:10%;" value="Add Ist Shift Users" onclick="user_fun('user_ist',this.id);"> <input type="button" class=""  data-id = "1" style="margin-left:10%;" name="use_secnd" id="use_secnd" value="Add Second Shift Users" onclick="user_fun('user_2nd',this.id);"></th></tr>
