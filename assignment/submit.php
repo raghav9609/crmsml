52cce3313a17d0d0a90928c1c5f2876f5707ab95
aakash <aakash@moneywide.com>
2023-12-15 12:58:18 +0530

    changes

--- assignment/submit.php
+++ assignment/submit.php
@@ -39,6 +39,6 @@ foreach ($arr as $user => $val) {
 
 // header("location:https://astechnos.com/crmsml/assignment/index.php?msg=1");
 
-echo '<script>window.location.href = https://astechnos.com/crmsml/assignment/index.php?msg=1;</script>';
+echo '<script>window.location.href = "https://astechnos.com/crmsml/assignment/index.php?msg=1";</script>';
 exit();
 ?>
