05e832abcb9a0763adf4f919fc4e0688ca437727
Sumit <sumit@moneywide.com>
2023-12-15 13:35:09 +0530

    as

--- query/update-details.php
+++ query/update-details.php
@@ -12,143 +12,89 @@ require_once "../include/helper.functions.php";
 		if(in_array($_REQUEST['loan_type'],array(63,57)) && $_REQUEST['occupation_id'] == 3){
 		$get_company_name = $_REQUEST['firm_name'];
 	}
-	$get_city_id = data_search('city',1);
+	$get_city_id = data_search('city');
 	//$company_array = data_search('company',1);
 	$city = searchValue($_REQUEST['city_name'],'city_name', $get_city_id);
-	$final_city_id = $get_city_id[$city]['city_id'];
-	$get_city_id_qry = mysqli_query($Conn1,"select main_city_id from lms_city where city_name  = '".$_REQUEST['city_name']."'");
-	$result_city_id_qry = mysqli_fetch_assoc($get_city_id_qry);
-	if($result_city_id_qry['main_city_id'] && is_numeric($result_city_id_qry['main_city_id'])){
-		$final_city_id = $result_city_id_qry['main_city_id'];
-	}
+	$final_city_id = $get_city_id[$city]['id'];
+	// $get_city_id_qry = mysqli_query($Conn1,"select main_city_id from lms_city where city_name  = '".$_REQUEST['city_name']."'");
+	// $result_city_id_qry = mysqli_fetch_assoc($get_city_id_qry);
+	// if($result_city_id_qry['main_city_id'] && is_numeric($result_city_id_qry['main_city_id'])){
+	// 	$final_city_id = $result_city_id_qry['main_city_id'];
+	// }
 	if($_REQUEST['work_city'] != ''){
 		$work_city = searchValue($_REQUEST['work_city'],'city_name', $get_city_id);
 	}
 	$comp_name_other = $comp_id = $hospital_name = $main_comp_category = $sub_comp_category = $sub_sub_comp_category = $state_comp_category = '';
-	$comp_id_qry = mysqli_query($Conn1,"select comp_id,group_id,main_company_id from pl_company where comp_name = '".mysqli_real_escape_string($Conn1,$get_company_name)."'  and is_removed = 0");
-    if(mysqli_num_rows($comp_id_qry) > 0){
-		$result_comp_id = mysqli_fetch_assoc($comp_id_qry);
-        $comp_id = $result_comp_id['comp_id'];
-		$main_company_id = $result_comp_id['main_company_id'];
-		if($main_company_id > 0 && is_numeric($main_company_id)){
-			$comp_id = $main_company_id;
-		}
-	}else{
-		$comp_name_other = $get_company_name;
-	}  
+	// $comp_id_qry = mysqli_query($Conn1,"select comp_id,group_id,main_company_id from pl_company where comp_name = '".mysqli_real_escape_string($Conn1,$get_company_name)."'  and is_removed = 0");
+    // if(mysqli_num_rows($comp_id_qry) > 0){
+	// 	$result_comp_id = mysqli_fetch_assoc($comp_id_qry);
+    //     $comp_id = $result_comp_id['comp_id'];
+	// 	$main_company_id = $result_comp_id['main_company_id'];
+	// 	if($main_company_id > 0 && is_numeric($main_company_id)){
+	// 		$comp_id = $main_company_id;
+	// 	}
+	// }else{
+	// 	$comp_name_other = $get_company_name;
+	// }  
 
-	if($_REQUEST['main_comp_category'] > 0 && is_numeric($_REQUEST['main_comp_category'])){
-		$comp_category = $_REQUEST['main_comp_category'];
-		if(in_array($comp_category,array(2,3,5,6))){
-			if(in_array($_REQUEST['company_sub_cat'],array(1,4))){
-				$sub_comp_category = $_REQUEST['company_sub_cat'];
-				$comp_id = 0;
-				if($comp_category == 2){
-					$comp_name_other = 'Indian Army';
-				}else if($comp_category == 3){
-					$comp_name_other = 'Indian Railway';
-				}else if($comp_category == 5){
-					$comp_name_other = 'Indian Navy';
-				}else if($comp_category == 6){
-					$comp_name_other = 'Indian Airforce';
-				}
-			}else{
-				$comp_category = '';
-			}
-		}else{
-			$comp_id = 0;
-			$comp_name_other = '';
-			$sub_comp_category = $_REQUEST['company_sub_cat'];
-			$sub_sub_comp_category = $_REQUEST['company_sub_sub_cat'];
-			$state_comp_category = $_REQUEST['comp_cat_state'];
-			 if($state_comp_category > 0 && is_numeric($state_comp_category) && $sub_comp_category == 2){
-				$comp_name_other = get_display_name("state",$state_comp_category);
-			 }else if($state_comp_category > 0 && is_numeric($state_comp_category) && $sub_comp_category == 1 && $comp_category == 1){
-				$comp_name_other = get_display_name("central_police",$state_comp_category);
-			 }
-			$comp_name_other .= " ".$get_company_name;
-		}
-	}
+	// if($_REQUEST['main_comp_category'] > 0 && is_numeric($_REQUEST['main_comp_category'])){
+	// 	$comp_category = $_REQUEST['main_comp_category'];
+	// 	if(in_array($comp_category,array(2,3,5,6))){
+	// 		if(in_array($_REQUEST['company_sub_cat'],array(1,4))){
+	// 			$sub_comp_category = $_REQUEST['company_sub_cat'];
+	// 			$comp_id = 0;
+	// 			if($comp_category == 2){
+	// 				$comp_name_other = 'Indian Army';
+	// 			}else if($comp_category == 3){
+	// 				$comp_name_other = 'Indian Railway';
+	// 			}else if($comp_category == 5){
+	// 				$comp_name_other = 'Indian Navy';
+	// 			}else if($comp_category == 6){
+	// 				$comp_name_other = 'Indian Airforce';
+	// 			}
+	// 		}else{
+	// 			$comp_category = '';
+	// 		}
+	// 	}else{
+	// 		$comp_id = 0;
+	// 		$comp_name_other = '';
+	// 		$sub_comp_category = $_REQUEST['company_sub_cat'];
+	// 		$sub_sub_comp_category = $_REQUEST['company_sub_sub_cat'];
+	// 		$state_comp_category = $_REQUEST['comp_cat_state'];
+	// 		 if($state_comp_category > 0 && is_numeric($state_comp_category) && $sub_comp_category == 2){
+	// 			$comp_name_other = get_display_name("state",$state_comp_category);
+	// 		 }else if($state_comp_category > 0 && is_numeric($state_comp_category) && $sub_comp_category == 1 && $comp_category == 1){
+	// 			$comp_name_other = get_display_name("central_police",$state_comp_category);
+	// 		 }
+	// 		$comp_name_other .= " ".$get_company_name;
+	// 	}
+	// }
 
-	if(in_array($_REQUEST['occupation_id'],array(2,3)) && $_REQUEST['gross_annual_receipt'] > 0){
-		$net_income = round($_REQUEST['gross_annual_receipt']/12);
-	}else{
-		$net_income = $_REQUEST['net_month_inc'];
-	}
-	if($_REQUEST['loan_type'] == 60){
-		$fieds_array = array(
-		'salu_id' => $_REQUEST['salutation'],
-		'name' => $_REQUEST['name'],
-		'mname' => $_REQUEST['mname'],
-		'lname' => $_REQUEST['lname'],
-		'dob' => $_REQUEST['dob'],
-		'email' => $_REQUEST['email'],
-		'city_id' => $final_city_id
-	);
-	$fieds_array_intt = array(
-		'pincode' => $_REQUEST['pin_code']
-	);
+    $net_income = $_REQUEST['net_month_inc'];
 
-	}else if(in_array($_REQUEST['loan_type'],array(11,57,63,71))){
-		$fieds_array = array(
-		'salu_id' => $_REQUEST['salutation'],
-		'name' => $_REQUEST['name'],
-		'mname' => $_REQUEST['mname'],
-		'lname' => $_REQUEST['lname'],
-		'dob' => $_REQUEST['dob'],
-		'email' => $_REQUEST['email'],
-		'maritalstatus' => $_REQUEST['maritalstatus'],
-		'occup_id' => $_REQUEST['occupation_id'],
-		'comp_id' => $comp_id,
-		'comp_name_other' => $comp_name_other,
-		'net_incm' => $net_income,
-		'bank_id' => $_REQUEST['main_acc'],
-		'comp_category' => $comp_category,
-		'sub_comp_category' => $sub_comp_category,
-		'sub_sub_comp_category' => $sub_sub_comp_category,
-		'govt_cat_state_id' => $state_comp_category,
-		'city_id' => $final_city_id,
-		'res_address' => trim($_REQUEST['address']),
-		'ofc_pincode' => trim($_REQUEST['ofc_pincode']),
-		'work_city' => $get_city_id[$work_city]['city_id'],
-		'offce_address' => trim($_REQUEST['offce_address']),
-		'ofc_email' => trim($_REQUEST['ofc_email'])
-
-	);
-	$fieds_array_intt = array(
-		'salary_pay_id' => $_REQUEST['slry_paid'],
-		'pincode' => $_REQUEST['pin_code'],
-		'profession_id' => $_REQUEST['profession'],
-		'cur_comp_wrk_exp'=> $_REQUEST['ccwe'],
-		'totl_wrk_exp'=> $_REQUEST['twe']
-	);
-	}else if(in_array($_REQUEST['loan_type'],array(56))){
-		$fieds_array = array(
-		'salu_id' => $_REQUEST['salutation'],
-		'name' => $_REQUEST['name'],
-		'mname' => $_REQUEST['mname'],
-		'lname' => $_REQUEST['lname'],
-		'dob' => $_REQUEST['dob'],
-		'comp_category'=>$comp_category,
-		'sub_comp_category'=>$sub_comp_category,
-		'sub_sub_comp_category'=>$sub_sub_comp_category,
-		'govt_cat_state_id'=>$state_comp_category,
-		'account_no' =>  $_REQUEST['account_no'],
-		'email' => $_REQUEST['email'],
-		'maritalstatus' => $_REQUEST['maritalstatus'],
-		'occup_id' => $_REQUEST['occupation_id'],
-		'comp_id' => $comp_id,
-		'comp_name_other' => $comp_name_other,
-		'net_incm' => $net_income,
-		'bank_id' => $_REQUEST['main_acc'],
-		'city_id' => $final_city_id,
-		'res_address' => trim($_REQUEST['address']),
-		'ofc_pincode' => trim($_REQUEST['ofc_pincode']),
-		'work_city' => $get_city_id[$work_city]['city_id'],
-		'offce_address' => trim($_REQUEST['offce_address']),
-		'ofc_email' => trim($_REQUEST['ofc_email'])
-
-	);
+        $fieds_array = array(
+            'salu_id' => $_REQUEST['salutation'],
+            'name' => $_REQUEST['name'],
+            'dob' => $_REQUEST['dob'],
+            'comp_category'=>$comp_category,
+            'sub_comp_category'=>$sub_comp_category,
+            'sub_sub_comp_category'=>$sub_sub_comp_category,
+            'govt_cat_state_id'=>$state_comp_category,
+            'account_no' =>  $_REQUEST['account_no'],
+            'email' => $_REQUEST['email'],
+            'maritalstatus' => $_REQUEST['maritalstatus'],
+            'occup_id' => $_REQUEST['occupation_id'],
+            'comp_id' => $comp_id,
+            'comp_name_other' => $comp_name_other,
+            'net_incm' => $net_income,
+            'bank_id' => $_REQUEST['main_acc'],
+            'city_id' => $final_city_id,
+            'res_address' => trim($_REQUEST['address']),
+            'ofc_pincode' => trim($_REQUEST['ofc_pincode']),
+            'work_city' => $get_city_id[$work_city]['city_id'],
+            'offce_address' => trim($_REQUEST['offce_address']),
+            'ofc_email' => trim($_REQUEST['ofc_email'])
+	    );
 	$fieds_array_intt = array(
 		'salary_pay_id' => $_REQUEST['slry_paid'],
 		'pincode' => $_REQUEST['pin_code'],
@@ -157,46 +103,7 @@ require_once "../include/helper.functions.php";
 		'totl_wrk_exp'=> $_REQUEST['twe'],
 		'saving_accounts_with' => implode(',',$_REQUEST['saving_acc_with_banks'])
 	);
-	}else if($_REQUEST['loan_type'] == 32){
-		$fieds_array = array(
-		'salu_id' => $_REQUEST['salutation'],
-		'name' => $_REQUEST['name'],
-		'mname' => $_REQUEST['mname'],
-		'lname' => $_REQUEST['lname'],
-		'dob' => $_REQUEST['dob'],
-		'email' => $_REQUEST['email'],
-		'city_id' => $final_city_id,
-		'res_address' => trim($_REQUEST['address'])
-	);
-	$fieds_array_intt = array(
-		'pincode' => $_REQUEST['pin_code']
-	);
-	}else{
-		$fieds_array = array(
-		'salu_id' => $_REQUEST['salutation'],
-		'name' => $_REQUEST['name'],
-		'mname' => $_REQUEST['mname'],
-		'lname' => $_REQUEST['lname'],
-		'dob' => $_REQUEST['dob'],
-		'comp_category'=>$comp_category,
-		'sub_comp_category'=>$sub_comp_category,
-		'sub_sub_comp_category'=>$sub_sub_comp_category,
-		'govt_cat_state_id'=>$state_comp_category,
-		'email' => $_REQUEST['email'],
-		'maritalstatus' => $_REQUEST['maritalstatus'],
-		'occup_id' => $_REQUEST['occupation_id'],
-		'comp_id' => $comp_id,
-		'comp_name_other' => $comp_name_other,
-		'net_incm' => $net_income,
-		'bank_id' => $_REQUEST['main_acc'],
-		'city_id' => $final_city_id,
-		'res_address' => trim($_REQUEST['address'])
-	);
-	$fieds_array_intt = array(
-		'salary_pay_id' => $_REQUEST['slry_paid'],
-		'pincode' => $_REQUEST['pin_code']
-	);
-	}
+	
 	$count = count($fieds_array);
 	$count_intt = count($fieds_array_intt);
 	$main_array = $intt_array = 0;
@@ -212,12 +119,7 @@ require_once "../include/helper.functions.php";
 	if(is_numeric($_REQUEST['alt_phone_no'])){
 		$query_to_update .= ",alt_phone=".$_REQUEST['alt_phone_no'];
 	}
-	if($_REQUEST['gl_in_past'] != "" && $_REQUEST['gl_inst'] != "") {
-		$query_to_update .= " , gold_loan_past_bank_id = '".$_REQUEST['gl_inst']."' ";
-	}
-	if($_REQUEST['when_loan'] != "") {
-		$query_to_update .= " , car_booked = '".$_REQUEST['when_loan']."' ";
-	}
+
 	$query_to_update .= " where id ='".replace_special($_REQUEST['cust_id'])."'";
 	$update_qry = mysqli_query($Conn1,$query_to_update) or die(mysqli_error($Conn1));
 	$query_to_update_intt = "update tbl_mint_cust_info_intt set ";
@@ -272,50 +174,9 @@ require_once "../include/helper.functions.php";
         return_data = '".json_encode($return_data)."',lead_score='".($final_lead_score*100)."',source=2,date=NOW()");
     }
     	
-}else if($_REQUEST['step'] == 2){
-	if(in_array($_REQUEST['loan_type'],array(51,52,54))){
-		$top_up_needed = 0;
-	if($_REQUEST['top_loan_amt'] > 0){
-		$top_up_needed = 1;
-	}
+} else if($_REQUEST['step'] == 2){
+    $property_identified = $_REQUEST['prop_identified'];
 
-	$get_city_id = data_search('city',1);
-	$city = searchValue($_REQUEST['prop_city_id'],'city_name', $get_city_id);
-	$final_city_id = $get_city_id[$city]['city_id'];
-	$get_city_id_qry = mysqli_query($Conn1,"select main_city_id from lms_city where city_name  = '".$_REQUEST['prop_city_id']."'");
-	$result_city_id_qry = mysqli_fetch_assoc($get_city_id_qry);
-	if($result_city_id_qry['main_city_id'] && is_numeric($result_city_id_qry['main_city_id'])){
-		$final_city_id = $result_city_id_qry['main_city_id'];
-	}
-	$prop_location_id =	1;
-	if($_REQUEST['udconst'] == 3 && ($_REQUEST['negative_ques'] < 40 ||  $_REQUEST['negative_ques'] > 50)){
-		$prop_location_id =	2;
-	}
-	}
-	if($_REQUEST['loan_type'] == 54 || $_REQUEST['loan_type'] == 52){
-		$property_identified = "Y";
-	}else{
-		$property_identified = $_REQUEST['prop_identified'];
-	}
-	
-	if($_REQUEST['loan_type'] == 57){
-        if($_REQUEST['annual_turnover'] > 0){
-            $get_annual_turnover_qry = mysqli_query($Conn1,"select bus_anl_id from tbl_bussiness_anl_trunover where turnover_num <= ".$_REQUEST['annual_turnover']." ORDER BY turnover_num DESC LIMIT 1");
-            $result_annual_turnover_qry = mysqli_fetch_assoc($get_annual_turnover_qry);
-            $annual_turnover_num = $result_annual_turnover_qry['bus_anl_id'];
-        }
-        if($_REQUEST['years_in_business'] > 0){
-            $get_ext_bus_qry = mysqli_query($Conn1,"select bus_ext_year_id from tbl_bussiness_extng_year where bus_num  <= ".$_REQUEST['years_in_business']." ORDER BY bus_num DESC LIMIT 1");
-            $result_ext_bus_qry = mysqli_fetch_assoc($get_ext_bus_qry);
-            $business_existing_num = $result_ext_bus_qry['bus_ext_year_id'];
-        }
-    }
-    if($_REQUEST['type_of_registration'] == 6){
-    	$gst_reg = 	1;
-	}
-	if($_REQUEST['loan_type'] == 57) {
-		$gst_reg = $_REQUEST['is_gst_yn'];
-	}
 	$fieds_array = array(
 		'score_flag' => 0,
 		'loan_nature' => $_REQUEST['nature_loan'],
