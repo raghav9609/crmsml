6a82681bfc496dce3737eb71f42289b2bce04d08
aakash <aakash@moneywide.com>
2023-12-19 15:28:21 +0530

    changes

--- assignment/submit.php
+++ assignment/submit.php
@@ -80,7 +80,14 @@ foreach ($seconduserValues as $user_id_array) {
     // $res_search = mysqli_num_rows($qry_search);
  
     if (!empty($res_search)){
-        
+        if ($res_search[0]['id'] == 78 ){
+            print_r($res_search);
+            echo "----";
+            echo "UPDATE crm_lead_assignment 
+            SET shift2_user_id = '" . $user_id . "'
+            WHERE id = '" . $res_search[0]['id'] . "'";
+            exit();
+        }
         $qry_update = mysqli_query($Conn1, "
         UPDATE crm_lead_assignment 
         SET shift2_user_id = '" . $user_id . "'
