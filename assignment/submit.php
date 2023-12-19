4f0321af0394400e5cfc38c518bc0cd9833cc7a7
aakash <aakash@moneywide.com>
2023-12-19 14:50:13 +0530

    changes

--- assignment/submit.php
+++ assignment/submit.php
@@ -76,7 +76,7 @@ foreach ($seconduserValues as $user_id_array) {
     // $qry_search = mysqli_query($Conn1, "SELECT * FROM crm_lead_assignment WHERE loan_type = '" . replace_special($_REQUEST['loan_type']) . "' AND min_loan_amount = '" . replace_special($_REQUEST['loan_frm']) . "' AND max_loan_amount = '" . replace_special($_REQUEST['loan_to']) . "' AND min_net_income = '" . replace_special($_REQUEST['salry_from']) . "' AND max_net_income = '" . replace_special($_REQUEST['salry_to']) . "' AND city_sub_group_id = '" . replace_special($_REQUEST['city_sub_group']) . "' AND shift1user_id = '" . $user_id . "'");
     $res_search = $db_handle->runQuery("SELECT * FROM crm_lead_assignment WHERE loan_type = '" . replace_special($_REQUEST['loan_type']) . "'AND min_loan_amount = '" . replace_special($_REQUEST['loan_frm']) . "'AND max_loan_amount = '" . replace_special($_REQUEST['loan_to']) . "'AND min_net_income = '" . replace_special($_REQUEST['salry_from']) . "'AND max_net_income = '" . replace_special($_REQUEST['salry_to']) . "'AND city_sub_group_id = '" . replace_special($_REQUEST['city_sub_group']) . "'AND (shift2_user_id = '" . $user_id . "' OR shift1user_id = '" . $user_id . "')");
     // $res_search = mysqli_num_rows($qry_search);
-    if ($res_search[0]['id'] == 66){
+    if ($res_search[0]['id'] == 69 || $res_search[0]['id'] == 70){
     print_r($res_search); 
     exit();}
     if (!empty($res_search)){
