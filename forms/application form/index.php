52e4575ab96605dec568c156ef7c02a9c83f027b
aakash <aakash@moneywide.com>
2023-12-22 12:20:51 +0530

    as

--- forms/application form/index.php
+++ forms/application form/index.php
@@ -1,10 +1,9 @@
 <?php
-echo "hi";
-exit();
-require_once(dirname(__FILE__) . '/../config/session.php');
-require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
-require_once(dirname(__FILE__) . '/../include/header.php');
-require_once(dirname(__FILE__) . '/../include/helper.functions.php');
+
+require_once(dirname(__FILE__) . '/../../config/session.php');
+require_once(dirname(__FILE__) . '/../../helpers/common-helper.php');
+require_once(dirname(__FILE__) . '/../../include/header.php');
+require_once(dirname(__FILE__) . '/../../include/helper.functions.php');
 
 $filter_data = [];
 
