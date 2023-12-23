373560192bcf87943cedf44d0013b8bcb55089b5
aakash <aakash@moneywide.com>
2023-12-23 21:20:30 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -134,7 +134,7 @@
                                 </div>
                             </div> -->
                             <div class="form-group col-xl-2 col-lg-4 col-md-6">
-                                <span class="fas fa-university"></span>
+                                <span class="fa-icon fa-building"></span>
                                 <input type="text" id="bank_name" name="bank_name" value="<?php echo ($name) ;?>" placeholder="Enter Bank Name" class="form-control alphaonly" maxlength="20" required>
                                 <label for="name" class="label-tag"> Bank Name</label>
                             </div>
