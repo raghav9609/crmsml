7c5a7fef4a18d1295059811f35d5f263ebe2bfe9
aakash <aakash@moneywide.com>
2023-12-23 20:46:43 +0530

    as

--- app/edit.php
+++ app/edit.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 
 require_once(dirname(__FILE__) . '/../include/header.php');
 
-// require_once(dirname(__FILE__) . '/../model/queryHelper.php');
+require_once(dirname(__FILE__) . '/../model/queryHelper.php');
 echo "hi111";
 exit();
 require_once(dirname(__FILE__) . '/../include/loader.php');
