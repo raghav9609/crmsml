bbd78cdd74b888772d093c97fe215a92b3a9d6e9
aakash <aakash@moneywide.com>
2023-12-23 20:05:04 +0530

    as

--- app/index.php
+++ app/index.php
@@ -243,7 +243,7 @@ $insurance = replace_special($_REQUEST['insurance']);
 $promo = replace_special($_REQUEST['promocode']);
 $ref_phone = replace_special($_REQUEST['ref_phone']);
 
-$qry_ex = "SELECT app.application_status as pre_login_status, cust.phone_no as phone, cust.name as name, app.id as app_id, cust.id as cust_id, cust.city_id as city_id,loan.value as loan_name,qry.crm_raw_data_id,app.crm_query_id,app.bank_id ,app.bank_application_no,qry.loan_type_id as loan_type,qry.loan_amount as required_loan_amt, app.login_date as login_date_on, app.sanction_date as sanction_date_on, app.disburse_date as first_disb_date_on, app.follow_up_date AS fup_date_on from crm_query_application as app JOIN crm_query as qry ON app.crm_query_id = qry.id Inner JOIN crm_customer as cust ON qry.crm_customer_id = cust.id INNER JOIN crm_masters as loan ON loan.id =  qry.loan_type_id ";
+$qry_ex = "SELECT app.application_status, cust.phone_no as phone, cust.name as name, app.id as app_id, cust.id as cust_id, cust.city_id as city_id,loan.value as loan_name,qry.crm_raw_data_id,app.crm_query_id,app.bank_id ,app.bank_application_no,qry.loan_type_id as loan_type,qry.loan_amount as required_loan_amt, app.login_date as login_date_on, app.sanction_date as sanction_date_on, app.disburse_date as first_disb_date_on, app.follow_up_date AS fup_date_on from crm_query_application as app JOIN crm_query as qry ON app.crm_query_id = qry.id Inner JOIN crm_customer as cust ON qry.crm_customer_id = cust.id INNER JOIN crm_masters as loan ON loan.id =  qry.loan_type_id ";
 
 // if(trim($bankers_name) != "") {
 //     $qry_ex .= " INNER JOIN banker_email_history on banker_email_history.case_id = c.case_id INNER JOIN banker_sms_history on banker_sms_history.case_id = c.case_id ";
@@ -433,7 +433,9 @@ while($exe = mysqli_fetch_array($res)){
 
 $cust_name  = $exe['name'];
 $phone  = $exe['phone'];
-// $pre_login_status  = $exe['pre_login_status'];
+$application_status_get  = $exe['application_status'];
+$get_application_status = get_dropdown('application_status','');
+print_r($get_application_status);
 // $sub_sub_status  = $exe['sub_sub_status'];
 // $cust_id = $exe['cust_id'];
 $loan_type = $exe['loan_type'];
