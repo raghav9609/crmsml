35ec2c7839768bbacdb8c03ec903a400cbfb899a
Sumit Raghav <sumit@moneywide.com>
2023-12-24 19:42:54 +0530

    chages

--- cron/lead-assignment.php
+++ cron/lead-assignment.php
@@ -20,8 +20,13 @@ foreach($fetch_unassign_leads as $key=>$value){
     }
 
     $data = array("city_sub_group_id"=>$city_sub_group_id,"net_income"=>$final_net_income,"loan_amount"=>$loan_amount,"loan_type_id"=>$loan_type_id);
-    $getLeadAssignemtn = $db_handle->runQuery($leadAssignmentClassexport->leadAssignment($data));
-    preArray($getLeadAssignemtn);
+    $getLeadAssignment = $db_handle->runQuery($leadAssignmentClassexport->leadAssignment($data));
+    $data_to_update = array("lead_assign_to"=>$getLeadAssignment['shift1user_id'],"query_id"=>$query_id);
+    $updateUserIdQuery = $db_handle->runQuery($leadAssignmentClassexport->userQueryID($data_to_update));
+    $data_to_insert = array("lead_assign_to"=>$getLeadAssignment['shift1user_id'],"query_id"=>$query_id);
+    $insertHistory = $db_handle->runQuery($leadAssignmentClassexport->leadAssignmentHistory($data_to_insert));
+    $updateLastLeadAssignOn = array("id"=>$getLeadAssignment['id']);
+    $updateLastLeadAssignmentBucket = $db_handle->runQuery($leadAssignmentClassexport->updateLastLeadAssignDate($updateLastLeadAssignOn));
 
 }
 
