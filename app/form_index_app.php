588b343c7ae3532296d5683a3857e81a6124e63c
aakash <aakash@moneywide.com>
2023-12-24 18:04:47 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -141,11 +141,11 @@
                                     <?php echo get_dropdown('application_status',''); ?>
                                     <label for="application_status" class="label-tag">Application Status</label>
                             </div>
-                                 <div class="form-group col-xl-2 col-lg-4 col-md-6">
-                                    <span class="fa-icon fa-building"></span>
-                                    <input type="text" id="applied_amount" name="applied_amount" value="<?php echo $applied_amount ;?>" placeholder="Enter Applied Amount" class="form-control alphaonly" maxlength="20" required>
-                                    <label for="applied_amount" class="label-tag"> Applied Amount</label>
-                                </div>
+                            <div class="form-group col-xl-2 col-lg-4 col-md-6">
+                                <span class="fa-icon fa-building"></span>
+                                <input type="text" id="applied_amount" name="applied_amount" value="<?php echo $applied_amount;?>" placeholder="Enter Applied Amount" class="form-control alphaonly" maxlength="20" <?php echo ($applied_amount != '') ? 'readonly' : ''; ?> required>
+                                <label for="applied_amount" class="label-tag"> Applied Amount</label>
+                            </div>
                                 <div class="form-group col-xl-2 col-lg-4 col-md-6">
                                     <input type="text" class="text form-control" name="dob" id="dob" maxlength="10" value="<?php echo $login_date != '0000-00-00'?$login_date:'';?>" placeholder="yyyy-mm-dd" />
                                     <label for="dob" class="label-tag ">Login Date</label>
