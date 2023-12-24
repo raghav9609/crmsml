6fb1208dd3f45220aa3faa84c5ba5b6a921250f8
Sumit Raghav <sumit@moneywide.com>
2023-12-24 18:19:49 +0530

    changes

--- cron/lead-assignment.php
+++ cron/lead-assignment.php
@@ -10,9 +10,16 @@ foreach($fetch_unassign_leads as $key=>$value){
     $query_id = $value['query_id'];
     $loan_type_id = $value['loan_type_id'];
     $qnet_income = $value['qnet_income'];
+    $cnet_income = $value['cnet_income'];
     $city_sub_group_id = $value['city_sub_group_id'];
 
-    $data = array("city_sub_group_id"=>$city_sub_group_id,"net_income"=>$qnet_income,"loan_amount"=>$loan_amount,"loan_type_id"=>$loan_type_id);
+    if($qnet_income > 0 && is_numeric($qnet_income)){
+        $final_net_income = $qnet_income;
+    }else{
+        $final_net_income = $cnet_income;
+    }
+
+    $data = array("city_sub_group_id"=>$city_sub_group_id,"net_income"=>$final_net_income,"loan_amount"=>$loan_amount,"loan_type_id"=>$loan_type_id);
 
    echo $getLeadAssignemtn =$leadAssignmentClassexport->leadAssignment($data);
 
