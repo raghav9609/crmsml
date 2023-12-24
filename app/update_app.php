31c55712d271b299640a01a186eca479e0c9cc61
aakash <aakash@moneywide.com>
2023-12-24 21:10:55 +0530

    as

--- app/update_app.php
+++ app/update_app.php
@@ -1,1 +1,26 @@
-<?php echo "hi"; ?>
+<?php 
+$bank_name = $_REQUEST['bank_name'];
+$application_status = $_REQUEST['application_status'];
+
+$applied_amount = $_REQUEST['applied_amount'];
+
+$login_date = $_REQUEST['login_date'];
+
+$sanction_amount = $_REQUEST['sanction_amount'];
+$sanction_date = $_REQUEST['sanction_date'];
+$disbursed_amount = $_REQUEST['disbursed_amount'];
+$disburse_date = $_REQUEST['disburse_date'];
+$remarks_by_user = $_REQUEST['remarks_by_user'];
+$remarks_by_bank = $_REQUEST['remarks_by_bank'];
+$bank_application_no = $_REQUEST['bank_application_no'];
+$follow_up_date = $_REQUEST['follow_up_date'];
+$follow_up_time = $_REQUEST['follow_up_time'];
+$follow_up_given_by = $_REQUEST['follow_up_given_by'];
+$tenure = $_REQUEST['tenure'];
+
+echo $tenure;
+
+
+
+
+?>
