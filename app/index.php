5e6bd2e69190da65e9ac0c9830dc4d0b7102bc8c
aakash <aakash@moneywide.com>
2023-12-24 17:20:38 +0530

    as

--- app/index.php
+++ app/index.php
@@ -1,11 +1,10 @@
 <?php
 echo "as";
-// require_once(dirname(__FILE__) . "/../config/session.php");
+//require_once(dirname(__FILE__) . "/../config/session.php");
 require_once(dirname(__FILE__) . "/../config/config.php");
 echo "as 1";
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
-echo "adskc";
-// require_once(dirname(__FILE__) . '/../include/header.php');
+require_once(dirname(__FILE__) . '/../include/header.php');
 echo "as 2";
 require_once "../include/helper.functions.php";
 require_once "../include/display-name-functions.php";
