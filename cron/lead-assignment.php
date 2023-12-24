614e26953f001fc8ca20a9ee0658cc6fc31e78d8
Sumit Raghav <sumit@moneywide.com>
2023-12-24 17:38:34 +0530

    changes

--- cron/lead-assignment.php
+++ cron/lead-assignment.php
@@ -2,7 +2,7 @@
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
-$fetch_unassign_leads = $db_handle->runQuery($Conn1,"select * from crm_query where is_lead_assign = 0 limit 50");
+$fetch_unassign_leads = $db_handle->runQuery("select * from crm_query where is_lead_assign = 0 limit 50");
 preArray($fetch_unassign_leads);
 
 
