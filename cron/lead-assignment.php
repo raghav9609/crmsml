6c7d7eade82f74110b13fd1d010aeb110505e9c7
Sumit Raghav <sumit@moneywide.com>
2023-12-24 19:45:15 +0530

    changes

--- cron/lead-assignment.php
+++ cron/lead-assignment.php
@@ -21,6 +21,9 @@ foreach($fetch_unassign_leads as $key=>$value){
 
     $data = array("city_sub_group_id"=>$city_sub_group_id,"net_income"=>$final_net_income,"loan_amount"=>$loan_amount,"loan_type_id"=>$loan_type_id);
     $getLeadAssignment = $db_handle->runQuery($leadAssignmentClassexport->leadAssignment($data));
+
+        preArray($getLeadAssignment);
+
     $data_to_update = array("lead_assign_to"=>$getLeadAssignment['shift1user_id'],"query_id"=>$query_id);
 
     echo $leadAssignmentClassexport->userQueryID($data_to_update);
