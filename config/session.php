28e2e276aa70547a155abead49bde7a74acd1bbb
aakash <aakash@moneywide.com>
2023-12-25 10:39:36 +0530

    as

--- config/session.php
+++ config/session.php
@@ -9,5 +9,5 @@ if(!is_numeric($user_id) || $user_id == 0 || !in_array($user_role,array(1,2,3,4)
   header("location:".$head_url."/logout.php");
     exit();
 }
-print_r($_SESSION);
+// print_r($_SESSION);
 ?>
