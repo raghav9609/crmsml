6d6409ebe9503ab575bdf07b0a8f4d50a658e2d7
aakash <aakash@moneywide.com>
2023-12-25 10:39:00 +0530

    as

--- config/session.php
+++ config/session.php
@@ -4,7 +4,7 @@ $user_id = $_SESSION['userDetails']['user_id'];
 $user_role = $_SESSION['userDetails']['role_id'];
 $show_external_api_data = 1;
 
-print_r($_SESSION);
+// print_r($_SESSION);
 if(!is_numeric($user_id) || $user_id == 0 || !in_array($user_role,array(1,2,3,4))){
   header("location:".$head_url."/logout.php");
     exit();
