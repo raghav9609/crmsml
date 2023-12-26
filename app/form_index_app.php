f74542b97e21ee1e8e40c9f07601d4b1ed6b1b44
aakash <aakash@moneywide.com>
2023-12-26 15:54:21 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -231,9 +231,9 @@ document.addEventListener('DOMContentLoaded', function () {
         var login_date = new Date(logindateInput.value);
         var sanction_date = new Date(sanctiondateInput.value);
         var disburse_date = new Date(disbursementInputdate.value);
-        alert(login_date);
-        alert(sanction_date);
-        alert(disburse_date);
+        // alert(login_date);
+        // alert(sanction_date);
+        // alert(disburse_date);
         if (disburse_date < login_date || disburse_date < sanction_date) {
             errormessageElement.textContent = 'Disbursement Date should not be smaller than Login Date and Sanction Date.';
         } else {
