b8241d24e2fb16d7f0fbecd093d2dc94816d5a95
Sumit Raghav <sumit@moneywide.com>
2023-12-26 11:53:03 +0530

    changes

--- calculators/calculate-eligibility.php
+++ calculators/calculate-eligibility.php
@@ -64,7 +64,7 @@ if($_REQUEST['calculate']){
 <html>
 <head>
      <title>Calculate Eligibility</title>
-     <link rel="stylesheet" type="text/css" href="<?php echo $head_url; ?>/assets/css/style.css">
+     <link rel="stylesheet" type="text/css" href="<?php echo $head_url; ?>/assets/css/style.css?v=1">
 </head>
 <body>
 <fieldset class='mall_30' style="height:200px;">
