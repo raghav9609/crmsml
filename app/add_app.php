87ff20ec91ee25ae98e74a6832ce8c61c11a4b03
aakash <aakash@moneywide.com>
2023-12-26 22:05:43 +0530

    as

--- app/add_app.php
+++ app/add_app.php
@@ -9,12 +9,37 @@
     <link rel="stylesheet" href="<?php echo $head_url; ?>/assets/css/multiselect.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $head_url; ?>/assets/css/style.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo $head_url; ?>/assets/css/jquery-ui.css" />
+    <style>
+    .fa-icon { 
+        font-size: 18px;
+    }
+    .fa-mobile {
+        font-size: 25px !important;
+    }
+    #form_step4 b.caret{
+        margin-left:215px!important;
+        display: inline;
+    }
+    #form_step4 .border-class{
+        font-size: 14px;
+        margin-left: 6px!important;
+        border-bottom: 1px solid #ced4da!important;
+        width: 100%!important;
+        }
+        #form_step4 .dropdown-menu {
+        left: -150px;
+        width: 241px;
+    }
+    .error-message {
+        color: red;
+    }
+    </style>
 </head>
 <body>
 <div class="pl-md-3 pl-2 pr-md-3 pr-2 col-12">
     
     <div class="gen-box white-bg">
-		<div class="col-md-12">
+    <div class="blue-bg col-12 font-weight-nb pb-2 pt-2 white font-20 brdr-top-gray pe-none" data-toggle="step1" id="switch_step1">
 			<a href="form_index_app.php"><input type="button" class="buttonsub cursor" value="Back"></a>
 			<h3>Add APplication </h3>
 			<form name="add_application" action="update.php" method="POST" autocomplete="OFF" enctype="multipart/form-data">
