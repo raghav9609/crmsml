a22d2f7c3636f7780d41c57656e3ae8723606796
Sumit <sumit@moneywide.com>
2023-12-26 14:12:45 +0530

    as

--- query/form_index.php
+++ query/form_index.php
@@ -245,7 +245,7 @@
                                 </div>
                                 <div class="form-group col-xl-2 col-lg-4 col-md-6 main_acc hidden">
                                     <span class="fa-icon fa-bank"></span>
-                                    <?php echo get_dropdown('13','main_acc',$main_account,'class="main_acc"'); ?>
+                                    <?php echo get_dropdown('2','main_acc',$main_account,'class="main_acc"'); ?>
                                     <label for="main_acc" class="label-tag">Main Account</label>
                                 </div>
                            
