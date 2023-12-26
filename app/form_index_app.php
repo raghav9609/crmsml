0193f64442c284384da55c172aaa78b830520289
aakash <aakash@moneywide.com>
2023-12-26 12:25:55 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -184,25 +184,26 @@
     });
 
     document.addEventListener('DOMContentLoaded', function() {
-    var appliedAmount = parseFloat(document.getElementById('applied_amount').value) || 0;
-    var sanctionAmount = parseFloat(document.getElementById('sanction_amount').value) || 0;
+        //amount 
+        var appliedAmount = parseFloat(document.getElementById('applied_amount').value) || 0;
+        var sanctionAmount = parseFloat(document.getElementById('sanction_amount').value) || 0;
 
-    var disbursementInput = document.getElementById('disbursed_amount');
+        var disbursementInput = document.getElementById('disbursed_amount');
 
-    var messageElement = document.createElement('span');
-    messageElement.className = 'error-message';
-    disbursementInput.parentNode.appendChild(messageElement);
+        var messageElement = document.createElement('span');
+        messageElement.className = 'error-message';
+        disbursementInput.parentNode.appendChild(messageElement);
 
-    disbursementInput.addEventListener('input', function() {
-        var disbursementAmount = parseFloat(disbursementInput.value) || 0;
+        disbursementInput.addEventListener('input', function() {
+            var disbursementAmount = parseFloat(disbursementInput.value) || 0;
 
-        if (disbursementAmount < appliedAmount || disbursementAmount > sanctionAmount) {
-            messageElement.textContent = 'Disbursement amount should be between Applied Amount and Sanction Amount.';
-        } else {
-            messageElement.textContent = '';
-        }
+            if (disbursementAmount < appliedAmount || disbursementAmount < sanctionAmount) {
+                messageElement.textContent = 'Disbursement amount should not be smaller than Applied Amount or Sanction Amount.';
+            } else {
+                messageElement.textContent = '';
+            }
+        });
     });
-});
     document.addEventListener('DOMContentLoaded', function () {
         var loginDate = new Date(document.getElementById('login_date').value);
         var sanctionDate = new Date(document.getElementById('sanction_date').value);
