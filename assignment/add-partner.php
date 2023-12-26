bcbc1be0fcc4e406325791f756356c6e5b92ec34
aakash <aakash@moneywide.com>
2023-12-26 21:06:12 +0530

    as

--- assignment/add-partner.php
+++ assignment/add-partner.php
@@ -6,10 +6,11 @@ require_once "../include/helper.functions.php";
 <div style="margin:0 auto; width:90%; padding:10px; background-color:#fff; height:800px;">
 <form method="POST" name="add_partner_form" action="submit.php">
 <table class="gridtable" style='margin-left:2%;width:80%;'>
-<tr><th colspan="2"><input type="submit" name="add" id = "add_app" class="buttonsub" style="margin-left:10%;" value="Add"> </th></tr>
+<tr><th colspan="2"><input type="submit" name="add" id = "add_app" class="buttonsub" style="margin-left:10%;" value="Add" onclick="validateSalaryRange()"> </th></tr>
 <tr><th>City Group: </th><td><?php echo get_dropdown('crm_master_city_sub_group','city_sub_group',"","required");?></td></tr>
 <tr><th>Loan Type: </th><td><?php echo get_dropdown(1,'loan_type',"","class='loan_type'");?></td></tr>
-<tr><th>Salary Range</td><td><input type="tel" id = "salry_from" name="salry_from" >  <input type="tel" id = "salry_to" name="salry_to" ><span id="salry_error" class="error-message"></span></td></tr>
+<div id="error-message" class="error-message"></div>
+<tr><th>Salary Range</td><td><input type="tel" id = "salry_from" name="salry_from" >  <input type="tel" id = "salry_to" name="salry_to" ></td></tr>
 <tr><th>Loan Amount</th><td><input type="tel" id = "loan_frm"  name="loan_frm" >  <input type="tel" id = "loan_to" name="loan_to" ></td></tr>
 <tr><th colspan="2"><input type="button" class="" name="use_frst" data-id="1" id="use_frst" style="margin-left:10%;" value="Add Users" onclick="user_fun('user_ist', this.id); user_fun('use_secnd', this.id);"></th></tr>
 <tr><th>Ist Shift User</th><td class="user_ist"> </td></tr>
@@ -20,39 +21,20 @@ require_once "../include/helper.functions.php";
 
 <script>
 
-document.addEventListener('DOMContentLoaded', function() {
-    var salry_fromInput = document.getElementById('salry_from');
-    var salry_toInput = document.getElementById('salry_to');
-    var add_app = document.getElementById('add_app');
-	alert(salry_fromInput);
-    // Create the error message span
-    var messageElement = document.createElement('span');
-    messageElement.className = 'error-message';
-    salry_fromInput.parentNode.appendChild(messageElement);
+function validateSalaryRange() {
+      var salaryFrom = parseFloat(document.getElementById('salry_from').value);
+      var salaryTo = parseFloat(document.getElementById('salry_to').value);
+      var errorMessage = document.getElementById('error-message');
 
-    function validatesalary() {
-        var salry_from = parseFloat(salry_fromInput.value) || 0;
-        var salry_to = parseFloat(salry_toInput.value) || 0;
-
-        if (salry_from >= salry_to) {
-            messageElement.textContent = 'Salary from should not be greater than or equal to salary to.';
-            add_app.setAttribute('disabled', 'disabled');
-        } else {
-            messageElement.textContent = '';
-            add_app.removeAttribute('disabled');
-        }
+      // Check if salary from is greater than salary to
+      if (isNaN(salaryFrom) || isNaN(salaryTo) || salaryFrom >= salaryTo) {
+        errorMessage.textContent = 'Error: Salary from must be less than Salary to';
+      } else {
+        errorMessage.textContent = '';
+        // Continue with your form submission logic here
+        // For example: document.getElementById('salaryForm').submit();
+      }
     }
-
-    salry_fromInput.addEventListener('input', function() {
-        validatesalary();
-    });
-
-    salry_toInput.addEventListener('input', function() {
-        validatesalary();
-    });
-
-    validatesalary();
-});
 var counter =0;
 
 function user_fun(type,id){
