f9a7aefbb657062058671f89d54adbfcf5f4ad65
aakash <aakash@moneywide.com>
2023-12-27 10:04:49 +0530

    as

--- app/add_app.php
+++ app/add_app.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+require_once(dirname(__FILE__) . '/../config/session.php');
+require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
+require_once(dirname(__FILE__) . '/../include/header.php');
+require_once(dirname(__FILE__) . '/../config/config.php');
+require_once "../include/helper.functions.php"; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
