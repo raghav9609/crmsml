7e79528192d0cec720aa4520a1a6790caa95a08e
aakash <aakash@moneywide.com>
2023-12-27 13:08:49 +0530

    as

--- app/add_app.php
+++ app/add_app.php
@@ -46,7 +46,7 @@ require_once "../include/helper.functions.php";
     <div class="gen-box white-bg">
     <div class="blue-bg col-12 font-weight-nb pb-2 pt-2 white font-20 brdr-top-gray pe-none" data-toggle="step1" id="switch_step1">
 			<a href="form_index_app.php"><input type="button" class="buttonsub cursor" value="Back"></a>
-			<h3 style = "background-color: #18375f;color:#ffffff">Add Application </h3>
+			<h3 style = "background-color: #18375f;color:#ffffff;text-align:center;">Add Application </h3>
 			<form name="add_application" action="update.php" method="POST" autocomplete="OFF" enctype="multipart/form-data">
 				<table class="gridtable" style="margin-left:2%;width:80%;"aid="maintable">
 					<tbody>
