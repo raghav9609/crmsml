4116cfc74571982014abb897309cf0b81e160305
Sumit Raghav <sumit@moneywide.com>
2023-12-31 11:17:32 +0530

    changes

--- email/fetch-template.php
+++ email/fetch-template.php
@@ -1,16 +1,16 @@
 <?php
-// require_once(dirname(__FILE__) . '/../config/session.php');
-// require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
-// require_once(dirname(__FILE__) . '/../include/helper.functions.php');
-require_once(dirname(__FILE__) . '/../model/queryHelper.php');
+require_once(dirname(__FILE__) . '/../config/session.php');
+require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
+require_once(dirname(__FILE__) . '/../include/helper.functions.php');
+// require_once(dirname(__FILE__) . '/../model/queryHelper.php');
 
 $template = replace_special($_REQUEST['temp']);
 $query_id = replace_special($_REQUEST['query_id']);
 
-$queryModelExport = new queryModel();
-echo $getQueryData = $queryModelExport->fetchDetails($query_id);
+// $queryModelExport = new queryModel();
+// echo $getQueryData = $queryModelExport->fetchDetails($query_id);
 
-//echo json_encode(array('html_temp'=>base64_encode($template_body),"subject"=>$subject));
+echo json_encode(array('html_temp'=>base64_encode($template_body),"subject"=>$subject));
 ?>
 
  
