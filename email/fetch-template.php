1ec7f05aa2fc2c392831c75a1158f2e207e1b7af
Sumit Raghav <sumit@moneywide.com>
2023-12-31 12:01:13 +0530

    changes

--- email/fetch-template.php
+++ email/fetch-template.php
@@ -10,8 +10,12 @@ $queryModelExport = new queryModel();
 $getQueryData = $db_handle->runQuery($queryModelExport->fetchQueryData($query_id));
 $getEmailData = $db_handle->runQuery("select * from crm_communication_template where id = ".$template." and is_active =1 and type = 1");
 
+
+
+// $final_template = str_replace("{loan_amount}",$getQueryData[0]['loan_amount'],str_replace("{agent_number}",$getQueryData[0]['agent_no'],str_replace("{agent_name}",$getQueryData[0]['user_name'],str_replace("{customer_name}",$getQueryData[0]['customer_name'],$getEmailData[0]['description']))));
+
 $final_template = str_replace(array("{customer_name}","{agent_name}","{agent_number}","{loan_amount}"),array($getQueryData[0]['customer_name'],$getQueryData[0]['user_name'],$getQueryData[0]['agent_no'],$getQueryData[0]['loan_amount']),$getEmailData[0]['description']);
-echo json_encode(array('html_temp'=>base64_encode($getEmailData[0]['description']),"subject"=>$getEmailData[0]['subject']));
+echo json_encode(array('html_temp'=>base64_encode($final_template),"subject"=>$getEmailData[0]['subject']));
 ?>
 
  
