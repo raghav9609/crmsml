0be992088a3491b8fb3c23e908d9aa038768f123
Sumit Raghav <sumit@moneywide.com>
2023-12-31 11:25:58 +0530

    chaneghs

--- email/fetch-template.php
+++ email/fetch-template.php
@@ -11,8 +11,6 @@ $query_id = replace_special($_REQUEST['query_id']);
 $queryModelExport = new queryModel();
 echo $getQueryData = $db_handle->runQuery($queryModelExport->fetchQueryData($query_id));
 $getEmailData = $db_handle->runQuery("select * from crm_communication_template where id = ".$template." and is_active =1 ");
-
-
 echo json_encode(array('html_temp'=>base64_encode($getEmailData[0]['template_name']),"subject"=>$getEmailData[0]['subject']));
 ?>
 
