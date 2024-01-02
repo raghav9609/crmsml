33ac2a1610de6529da21afd7a1b659bd02eea244
aakash <aakash@moneywide.com>
2024-01-02 14:19:39 +0530

    as

--- upload-csv/add.php
+++ upload-csv/add.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // echo "jjiiii";
         // exit();
         $chek_data  = "select * from crm_raw_data where phone_no = '".$phone_no[$i]."'";
-        // echo $chek_data;
+        echo $chek_data;
         // $chek_data1 = $db_handle->runQuery($chek_data);
         $res_qry_get = mysqli_query($Conn1,$chek_data);
         $res_qry = mysqli_fetch_array($res_qry_get);
