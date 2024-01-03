7f8140a64e9b4a55a2d5439be37f320926d5111d
aakash <aakash@moneywide.com>
2024-01-03 12:40:56 +0530

    as

---
+++ query/cust_details.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Details</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link href="<?php echo $head_url; ?>/assets/css/grid-form.css?v=1.1" rel="stylesheet">
+</head>
+<body>
+    <div class="pl-md-3 pl-2 pr-md-3 pr-2 col-12">
+        <div class="gen-box white-bg">
+        <div class="blue-bg col-12 font-weight-nb pb-2 pt-2 white font-20 brdr-top-gray pe-none" data-toggle="step1" id="switch_step1">
+            <span id="text_step1"></span> Customer Details<div id="error-message" class="error-message"></div></div>    
+                <form action="" class="form-step col-12" autocomplete="off" id="form_step1">        
+                    <div class="row div-width">
+
+                    </div>
+                </form>
+        </div>
+        </div>
+    </div>
+    
+</body>
+</html>
