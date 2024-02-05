fdb59759d4c655f1a7ca2e4f1968300aae43ba5f
Sumit <raghav9609@gmail.com>
2024-02-05 12:52:39 +0530

    chnages

--- email/send-email-process.php
+++ email/send-email-process.php
@@ -1,9 +1,9 @@
 <?php
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
+echo "qwe";
 require_once(dirname(__FILE__) . '/../include/class.mailer.php');
 echo "Hellllloooooooo";      
-
 $email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
 $cc_email = explode(",",$_REQUEST['cc_recipient_email']);
 $sender_email = $_REQUEST['sender_email'];
@@ -12,8 +12,6 @@ $description = $_REQUEST['description'];
 $temp_id = $_REQUEST['template'];											
 $query_id = $_REQUEST['query_id']; 
 if($email != ''){
-
-    
 $recep_mail = $email;
 $replytomail = array();
 $cctomail = $cc_email;
