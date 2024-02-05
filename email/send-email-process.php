20b7ead30e26430f8b76b88f4c5cf7aef29b79e4
Sumit <raghav9609@gmail.com>
2024-02-05 12:55:32 +0530

    changes

--- email/send-email-process.php
+++ email/send-email-process.php
@@ -1,7 +1,6 @@
 <?php
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
-echo "qwe";
 require_once(dirname(__FILE__) . '/../include/class.mailer.php');
 echo "Hellllloooooooo";      
 $email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
