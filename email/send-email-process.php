cc5d454c09af5f27d59be3ef4cc941532a3cfc0b
Sumit <raghav9609@gmail.com>
2024-02-05 13:01:13 +0530

    changes

--- email/send-email-process.php
+++ email/send-email-process.php
@@ -1,22 +1,21 @@
 <?php
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
-echo dirname(__FILE__) . '/../include/class.mailer.php';
 require_once(dirname(__FILE__) . '/../include/class.mailer.php');
 echo "Hellllloooooooo";      
-$email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
-$cc_email = explode(",",$_REQUEST['cc_recipient_email']);
-$sender_email = $_REQUEST['sender_email'];
-$subject = $_REQUEST['subject'];									
-$description = $_REQUEST['description'];
-$temp_id = $_REQUEST['template'];											
-$query_id = $_REQUEST['query_id']; 
-if($email != ''){
-$recep_mail = $email;
-$replytomail = array();
-$cctomail = $cc_email;
-mailSend($recep_mail,$cctomail,$replytomail,$subject,htmlspecialchars_decode($description));
-}
+// $email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
+// $cc_email = explode(",",$_REQUEST['cc_recipient_email']);
+// $sender_email = $_REQUEST['sender_email'];
+// $subject = $_REQUEST['subject'];									
+// $description = $_REQUEST['description'];
+// $temp_id = $_REQUEST['template'];											
+// $query_id = $_REQUEST['query_id']; 
+// if($email != ''){
+// $recep_mail = $email;
+// $replytomail = array();
+// $cctomail = $cc_email;
+// mailSend($recep_mail,$cctomail,$replytomail,$subject,htmlspecialchars_decode($description));
+// }
 
 
 // $qrysenddet = "INSERT INTO tbl_mint_mail_detail set mail_id ='".$mail_id."',subject='".$subject."',description='".htmlspecialchars_decode($description)."',cc_mail='".$cc_email."',sender_mail='info@myloancareindia.in'";
