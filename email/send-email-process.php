a5c524e1246ba530539b2edcfbf9de76bbcc0c4f
Sumit <raghav9609@gmail.com>
2024-02-05 11:56:32 +0530

    chnages

--- email/send-email-process.php
+++ email/send-email-process.php
@@ -2,33 +2,18 @@
 require_once "../../include/check-session.php";
 require_once "../../include/config.php";
 require_once "../../include/class.mailer.php";
-//require_once("../mailer/class.phpmailer.php");
 
-$email = $_REQUEST['recipient_email'];
-$cc_email = $_REQUEST['cc_recipient_email'];
+$email = array("raghav9609@gmail.com") //explode(",",$_REQUEST['recipient_email']);
+$cc_email = explode(",",$_REQUEST['cc_recipient_email']);
 $sender_email = $_REQUEST['sender_email'];
 $subject = $_REQUEST['subject'];									
 $description = $_REQUEST['description'];
 $temp_id = $_REQUEST['template'];											
 $query_id = $_REQUEST['query_id'];       
-$querysend = "INSERT INTO `tbl_mint_mail_new` (`temp_id`,`query_id`,`case_id`,`date`,`time`) VALUES ('".$temp_id."', '".$query_id."','".$case_id."',CURDATE(),CURTIME())";
-$resfrm = mysqli_query($Conn1,$querysend);
-if($case_id != "")
-{
-$query_mail = "select * from tbl_mint_mail_new where case_id = '".$case_id."' order by mail_id desc";
-}
-else if($query_id != ""){
-$query_mail = "select * from tbl_mint_mail_new where query_id = '".$query_id."' order by mail_id desc";
-}
-$exe_mail   = mysqli_query($Conn1,$query_mail);
-$res_mail   =  mysqli_fetch_array($exe_mail); 
-$mail_id = $res_mail['mail_id'];
 if($email != ''){
-	
-
-$recep_mail = array($email);
-$replytomail = array("care@myloancare.in");
-$cctomail = array($cc_email);
+$recep_mail = $email;
+$replytomail = array();
+$cctomail = $cc_email;
 mailSend($recep_mail,$cctomail,$replytomail,$subject,htmlspecialchars_decode($description));
 }
 
---
+++ include/class.mailer.php
@@ -0,0 +1,45 @@
+<?php
+$php_mailer_added = 1;
+require_once "PHPMailer/PHPMailerAutoload.php";
+function mailSend($recepitientMail,$ccMail,$replyMail,$subject,$body){
+	global $Conn1;
+		$mail = new PHPMailer();
+			$mail->SMTPOptions = array(
+				'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+			)
+		);
+		$mail->IsSMTP();
+        $mail->SMTPDebug = 0; 
+        $mail->SMTPAuth = true; // authentication enabled
+        $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+			$mail->Host = 'smtp.gmail.com';
+			$mail->Port = 465;
+			$mail->Username = 'Care@switchmyloan.in';
+			$mail->Password = 'SML2023@123';
+		
+		foreach($recepitientMail as $recptomail){
+			$mail->AddAddress($recptomail);
+		} 
+		foreach($ccMail as $cctomail){
+			$mail->AddCC($cctomail);
+		}$mail->AddBCC("dump@myloancareindia.in");
+		foreach($replyMail as $replytomail){
+			$mail->AddReplyTO($replytomail);
+		}
+		//$mail->AddBCC("sumit@myloancareindia.in");
+		$mail->SetFrom('info@myloancaremail.in', 'MyLoanCare');
+        $mail->Subject =$subject;
+        $mail->Body = $body;
+        $mail->IsHTML(true);	
+		if($mail->Send()){
+			$message = "Message has been sent";
+		}else{
+			$message = "Not sent";
+		}
+		$insert = mysqli_query($Conn1,"insert into crm_mail_send_history set email_to = '".implode(',',$recepitientMail)."',cc_email='".implode(',',$ccMail)."',reply_email='".implode(',',$replyMail)."',subject='".base64_encode($subject)."',mail_body='".base64_encode($body)."',sender_email='info@myloancareindia.in',date=NOW(),status_from_mailer='".$message."'");
+		return $message;
+}
+?>
