229222d8ab7a8ab9d761554bf6e6c7661fec447c
Sumit <raghav9609@gmail.com>
2024-02-05 13:44:40 +0530

    as

--- include/PHPMailer/PHPMailerAutoload.php
+++ include/PHPMailer/PHPMailerAutoload.php
@@ -22,16 +22,18 @@
  * @param string $classname The name of the class to load
  */
 
- echo "dssd";
+ 
+function PHPMailerAutoload($classname)
+{
+    //Can't use __DIR__ as it's only in PHP 5.3+
+    $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'class.'.strtolower($classname).'.php';
+    if (is_readable($filename)) {
+        require $filename;
+    }
+}
+
+echo "dssd";
  die();
-// function PHPMailerAutoload($classname)
-// {
-//     //Can't use __DIR__ as it's only in PHP 5.3+
-//     $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'class.'.strtolower($classname).'.php';
-//     if (is_readable($filename)) {
-//         require $filename;
-//     }
-// }
 
 // if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
 //     //SPL autoloading was introduced in PHP 5.1.2
