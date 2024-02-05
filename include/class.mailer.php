5d2f46a3d37722bdd5b1bc6aa9d35a86695aeadf
Sumit <raghav9609@gmail.com>
2024-02-05 13:31:22 +0530

    as

--- include/class.mailer.php
+++ include/class.mailer.php
@@ -1,6 +1,7 @@
 <?php
 $php_mailer_added = 1;
-require_once "PHPMailer/PHPMailerAutoload.php";
+//require_once "PHPMailer/PHPMailerAutoload.php";
+require_once(dirname(__FILE__) . '/../PHPMailer/PHPMailerAutoload.php');
 function mailSend($recepitientMail,$ccMail,$replyMail,$subject,$body){
 		$mail = new PHPMailer();
 			$mail->SMTPOptions = array(
