e2b27274e9eb6cd075cac1d9f20b514a24214531
aakash <aakash@moneywide.com>
2024-02-05 17:09:35 +0530

    as

--- query/city_name.php
+++ query/city_name.php
@@ -1,20 +1,19 @@
 <?php
-echo "hiii";
-// $no_head = 1;
-// $city_id = 0;
-// if($_REQUEST['term'] != ''){
-//     require_once(dirname(__FILE__) . '/../config/config.php');
-// 	require_once(dirname(__FILE__) . '/../model/queryHelper.php');
-//     $searchTerm = $_REQUEST['term'];
-//     $get_query   = new queryModel();
-//     $qry        = $get_query->getCityRecord($searchTerm);
-//     $db_handle  = new DBController();
-//     $data       = $db_handle->runQuery($qry);
+$no_head = 1;
+$city_id = 0;
+if($_REQUEST['term'] != ''){
+    require_once(dirname(__FILE__) . '/../config/config.php');
+	require_once(dirname(__FILE__) . '/../model/queryHelper.php');
+    $searchTerm = $_REQUEST['term'];
+    $get_query   = new queryModel();
+    $qry        = $get_query->getCityRecord($searchTerm);
+    $db_handle  = new DBController();
+    $data       = $db_handle->runQuery($qry);
 
-// 	$return_data = [];
-//     foreach($data as $key => $value) {
-//         $return_data[] = $value['city_name'];
-//     }
-//     echo json_encode($return_data);
-// }
+	$return_data = [];
+    foreach($data as $key => $value) {
+        $return_data[] = $value['city_name'];
+    }
+    echo json_encode($return_data);
+}
 ?>
