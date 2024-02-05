15810250309e718def54611f411bc81d84a630d9
Sumit <raghav9609@gmail.com>
2024-02-05 13:36:07 +0530

    as

--- email/send-email-process.php
+++ email/send-email-process.php
@@ -1,6 +1,8 @@
 <?php
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
+echo "ssdsdsa";
+die();
 require_once(dirname(__FILE__) . '/../include/class.mailer.php');
 echo "Hellllloooooooo";      
 // $email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
