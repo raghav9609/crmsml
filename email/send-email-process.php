fdf18067bdb947dd5d45907b2c9fa3d07bc6e594
Sumit <raghav9609@gmail.com>
2024-02-05 13:11:09 +0530

    changes

--- email/send-email-process.php
+++ email/send-email-process.php
@@ -3,17 +3,17 @@ require_once(dirname(__FILE__) . '/../config/session.php');
 //require_once(dirname(__FILE__) . '/../config/config.php');
 require_once "../include/class.mailer.php";
 echo "Hellllloooooooo";      
-$email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
-$cc_email = explode(",",$_REQUEST['cc_recipient_email']);
-$sender_email = $_REQUEST['sender_email'];
-$subject = $_REQUEST['subject'];									
-$description = $_REQUEST['description'];
-$temp_id = $_REQUEST['template'];											
-$query_id = $_REQUEST['query_id']; 
-if($email != ''){
-$recep_mail = $email;
-$replytomail = array();
-$cctomail = $cc_email;
+// $email = array("raghav9609@gmail.com"); //explode(",",$_REQUEST['recipient_email']);
+// $cc_email = explode(",",$_REQUEST['cc_recipient_email']);
+// $sender_email = $_REQUEST['sender_email'];
+// $subject = $_REQUEST['subject'];									
+// $description = $_REQUEST['description'];
+// $temp_id = $_REQUEST['template'];											
+// $query_id = $_REQUEST['query_id']; 
+// if($email != ''){
+// $recep_mail = $email;
+// $replytomail = array();
+// $cctomail = $cc_email;
 //mailSend($recep_mail,$cctomail,$replytomail,$subject,htmlspecialchars_decode($description));
 }
 
