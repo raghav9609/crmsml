f3912bfb1a6da7b845f1b361142f5725579be5bf
aakash <aakash@moneywide.com>
2024-02-05 16:08:21 +0530

    as

--- query/edit.php
+++ query/edit.php
@@ -106,7 +106,7 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
     $ofc_email = $result_cust_data['ofc_email'];
     $work_city = $result_cust_data['work_city'];
     $maritalstatus = $result_cust_data['maritalstatus'];
-    $ofc_city_name = get_display_name("city_name", $work_city);
+   echo "hii".$ofc_city_name = get_display_name("city_name", $work_city);
     $curDate = currentDate();
     $result_cust_data['cur_comp_wrk_exp'];
     $ccweget = dateDiff($curDate,$result_cust_data['cur_comp_wrk_exp'],1);
