344c2931f692f5fecd868438c0b8b389c521d7b6
Sumit <raghav9609@gmail.com>
2024-02-06 16:19:28 +0530

    as

--- getdetails/index.php
+++ getdetails/index.php
@@ -4,6 +4,6 @@ require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once(dirname(__FILE__) . '/../include/helper.functions.php');
 
 echo "anu";
-$_POST = file_get_contents('php://input');
-print_r($_POST['name']);
+$post = file_get_contents('php://input');
+print_r($post);
 ?>
