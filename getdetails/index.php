b12ee44a34ceb1b8c296f55abf3c1eb75b60e103
Sumit <raghav9609@gmail.com>
2024-02-06 16:03:24 +0530

    as

---
+++ getdetails/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once(dirname(__FILE__) . '/../config/config.php');
+require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
+require_once(dirname(__FILE__) . '/../include/helper.functions.php');
+
+echo "anu";
+
+?>
