efd963765d1d95951dccb88112e24d116cb9f6ef
Sumit <raghav9609@gmail.com>
2024-02-07 17:45:10 +0530

    as

--- offers/index.php
+++ offers/index.php
@@ -6,7 +6,7 @@ require_once(dirname(__FILE__) . '/../config/config.php');
 require_once(dirname(__FILE__) . '/../include/helper.functions.php');
 require_once(dirname(__FILE__) . '/../include/display-name-functions.php');
  
-$qry1 = "select * from crm_masters where crm_masters_code_id = 2 and is_active = 1 ";
+$qry1 = "select * from crm_masters where crm_masters_code_id = 10 and is_active = 1 ";
 
 $res = mysqli_query($Conn1, $qry1) or die("Error: " . mysqli_error($Conn1));
 $recordcount = mysqli_num_rows($res); 
@@ -14,9 +14,9 @@ if ($recordcount > 0) {
     $record = 0;
     while ($exe_form = mysqli_fetch_array($res)) {
         $record++;
-        if ($record > 10) {
-             continue;
-        }
+        // if ($record > 10) {
+        //      continue;
+        // }
         $data_bnk[] = '<input type ="checkbox"  name = "check_bank_'.$exe_form['id'].'" id = "check_bank_'.$exe_form['id'].'" value ="'.$exe_form['id'].'">'.$exe_form['value'];
     }
     echo implode($data_bnk);
