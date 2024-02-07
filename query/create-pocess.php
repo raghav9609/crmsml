e67a146090fc6da5fc459cfab7a3a690fa40402e
Sumit <raghav9609@gmail.com>
2024-02-07 18:09:48 +0530

    as

---
+++ query/create-pocess.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once(dirname(__FILE__) . '/../config/session.php');
+require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
+require_once(dirname(__FILE__) . '/../config/config.php');
+require_once "../include/helper.functions.php";
+require_once "../include/display-name-functions.php";
+
+?>
