b8c1e697bc8be7396a29b7d3530149e341dd1e20
Nishant Bhardwaj <nishant@myloancareindia.in>
2024-02-07 15:55:55 +0530

    changes

--- sms/send-sms-process.php
+++ sms/send-sms-process.php
@@ -5,10 +5,10 @@ require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once(dirname(__FILE__) . '/../include/class.mailer.php');
 
 $subject = $_REQUEST['subject'];									
-$description = urlencode($_REQUEST['description']);
+$description = ($_REQUEST['description']);
 $temp_id = $_REQUEST['template'];											
 $query_id = $_REQUEST['query_id']; 
- echo $url= 'https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91'.$subject.'&Text='.$description.'&async=1';
+echo $url= 'https://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=385302&username=9920706289&password=SML2021@123&senderid=SML&To=91'.$subject.'&Text='.$description.'&async=1';
  $response = curl_get_helper($url);
 $qry = "INSERT INTO crm_communication_history SET query_id='".$query_id."',type = 2, communication_id = '".$subject."',response='".base64_encode($response)."',description = '".base64_encode($description)."'";
 $insert_comm = mysqli_query($Conn1,$qry);
