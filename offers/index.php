8df2c0aa5b47c34b00aed9fe7ccaa749297b57d2
Sumit <raghav9609@gmail.com>
2024-02-07 17:30:33 +0530

    as

--- offers/index.php
+++ offers/index.php
@@ -8,6 +8,20 @@ require_once(dirname(__FILE__) . '/../include/display-name-functions.php');
  
 $qry1 = "select * from crm_masters where crm_masters_code_id = 2 and is_active = 1 ";
 
+$res = mysqli_query($Conn1, $qry1) or die("Error: " . mysqli_error($Conn1));
+$recordcount = mysqli_num_rows($res); 
+if ($recordcount > 0) {
+    $record = 0;
+    while ($exe_form = mysqli_fetch_array($res)) {
+        $record++;
+        if ($record > 10) {
+             continue;
+        }
+        $data_bnk[] = '<input type ="checkbox" name = "check_bank_'.$$exe_form['id'].'"  id = "check_bank_'.$exe_form['id'].'" value ="">'.$exe_form['value'];
+    }
+    pint_($data_bnk);
+
+
 
 ?>
 <!DOCTYPE html>
