d0165be6746287e135ce661ef7daf6d78334fea8
Sumit <raghav9609@gmail.com>
2024-02-07 17:32:19 +0530

    as

--- offers/index.php
+++ offers/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
-require_once(dirname(__FILE__) . '/../include/header.php');
+//require_once(dirname(__FILE__) . '/../include/header.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
 require_once(dirname(__FILE__) . '/../include/helper.functions.php');
 require_once(dirname(__FILE__) . '/../include/display-name-functions.php');
