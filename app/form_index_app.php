e49b1ed4125ce23dad765015dda9cfa2249a82e8
aakash <aakash@moneywide.com>
2024-02-08 17:24:57 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -408,7 +408,7 @@ $recordcount = mysqli_num_rows($res);
 					}
                     echo "<a class='page gradient' href='javascript:void;'>".$page."</a>";
                     if($recordcount > $display_count) {
-                        echo "<a class='page gradient' href='app/edit.php?page=".($page+1) ."'>Next</a>";
+                        echo "<a class='page gradient' href='app/edit.php?app_id='".urlencode(base64_encode($app_id))."page=".($page+1) ."'>Next</a>";
                     }
                     ?></td>
             </tr>
