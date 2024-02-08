3509f4d063f0435a6909d5452c9de34af7f0a8ad
aakash <aakash@moneywide.com>
2024-02-08 14:56:46 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -71,11 +71,14 @@
 <body>
 
     <!-- <main>  -->
+        
     <div class="pl-md-3 pl-2 pr-md-3 pr-2 col-12">
     
     <div class="gen-box white-bg">
     <div class="blue-bg col-12 font-weight-nb pb-2 pt-2 white font-20 brdr-top-gray pe-none" data-toggle="step1" id="switch_step1">
-        <span id="text_step1"></span> Application Details<div id="error-message" class="error-message"></div></div>    
+        <span id="text_step1"></span> Application Details<div id="error-message" class="error-message"></div></div> 
+        <?php// while($exe_form) ?>   
+        <?php print_r($exe_form);?>
         <form action="update_app.php" class="form-step col-12" autocomplete="off" id="form_step1">
                       
             <div class="row div-width">
