5114d301847a6c146c398dd16466a7d8e3739152
Sumit <raghav9609@gmail.com>
2024-02-08 12:11:04 +0530

    changes

--- assets/js/query-follow-up.js
+++ assets/js/query-follow-up.js
@@ -9,6 +9,19 @@ $(document).ready(function () {
     });
 
     $( "#ad_query" ).on( "click", function() {
-        alert("Hello");
+        var f_stats_val = $("#f_stats").val();
+        var fol_date = $("#fol_date").val();
+        var fol_time = $("#fol_time").val();
+        var folow_given = $("#folow_given").val();
+        if(f_stats_val == '' || f_stats_val == 0){
+            alert("Fill Mandatory Fields");
+        }else if((f_stats_val == 2 || f_stats_val == 3 || f_stats_val == 4 || f_stats_val == 8 || f_stats_val == 9 || f_stats_val == 5) && (fol_date == '' || fol_time == "")){
+            alert("Fill Mandatory Fields");
+        }else if(folow_given == '' || folow_given == 0){
+            alert("Fill Mandatory Fields");
+        }else{
+            alert("Hello");
+        }
+        
       } );
 });
