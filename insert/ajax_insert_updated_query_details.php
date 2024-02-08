c936bb957ba97cf4e7d3c99344f21d69e4987fa0
aakash <aakash@moneywide.com>
2024-02-08 10:34:19 +0530

    as

--- insert/ajax_insert_updated_query_details.php
+++ insert/ajax_insert_updated_query_details.php
@@ -1,12 +1,8 @@
 <?php
-echo "hih";
 require_once "../config/config.php";
-echo "hi";
 require_once "../include/helper.functions.php";
-echo "hi1";
 require_once "../include/display-name-functions.php";
-echo "hi2";
-exit();
+
 /* if($user == '173'){
 echo "select * from tbl_updated_query_details where query_id='".$qryyy_id."'";
 } */
