1c00c79b80c69a8b802311c3f68441062f0037f6
aakash <aakash@moneywide.com>
2024-02-08 10:41:47 +0530

    as

--- insert/ajax_insert_updated_query_details.php
+++ insert/ajax_insert_updated_query_details.php
@@ -14,11 +14,11 @@ $return_html = "";
 // tbl_updated_query_details.bus_anl_trnover LEFT JOIN tbl_bussiness_extng_year ON tbl_bussiness_extng_year.bus_ext_year_id = tbl_updated_query_details.bus_ext_year WHERE tbl_mint_query.query_id = '".$qryyy_id."'");
 $qry_get_data = "select * from crm_raw_data as raw_data left join crm_query as qry on raw_data.id = qry.crm_raw_data_id where qry.id = '".$qryyy_id."'";
 echo $qry_get_data;
-$numrow = mysqli_num_rows($qry_get_data);
-echo "hii".$numrow;
-exit();
-if(mysqli_num_rows($qry_get_data) > 0) {
-    $res_data = mysqli_fetch_array($qry_get_data);
+$res = mysqli_query($Conn1, $qry);
+$recordcount = mysqli_num_rows($res);
+print_r($recordcount);
+if($recordcount > 0) {
+    $res_data = mysqli_fetch_array($res);
     print_r($res_data);
     exit();
     $company_nm = "";
