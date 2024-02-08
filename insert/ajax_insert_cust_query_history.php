9c95d67722adbf3ee17a6cc5bf14428e87a2c402
aakash <aakash@moneywide.com>
2024-02-08 13:01:06 +0530

    as

--- insert/ajax_insert_cust_query_history.php
+++ insert/ajax_insert_cust_query_history.php
@@ -1,6 +1,6 @@
 <?php
 $slave = 1;
-require_once "../../include/config.php";
+require_once "../../config/config.php";
 require_once "../../include/helper.functions.php";
 require_once "../../include/display-name-functions.php";
 $type = $_REQUEST['type'];
