0ca7d5e074cd6ea98d7ed626ba13e515977b10dc
aakash <aakash@moneywide.com>
2024-02-08 18:23:26 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -169,14 +169,15 @@
                         <!-- <input type="text" id="follow_up_given_by" name="follow_up_given_by" value="<?php echo $follow_up_given_by;?>" placeholder="Enter Follow Up Given By" class="form-control  valid" maxlength="20" <?php echo ($follow_up_given_by != '') ? 'readonly' : ''; ?> required>
                         <label for="dob" class="label-tag ">Follow Up Given BY</label>
                         <span class='green' id='age' style="position: absolute;top: 100%;background: transparent;color: green;left: 15px;"></span> -->
-                        
+                        <span class='green' id='age' style="position: absolute;top: 100%;background: transparent;color: green;left: 15px;"></span> 
+                        <label for="follow_up_given_by" class="label-tag">Follow Up Given By</label>
                         <select id="follow_up_given_by" name="follow_up_given_by" <?php echo ($follow_up_given_by != '') ? 'readonly' : ''; ?> >
                             <option value="0">Select</option>
                             <option value="1" <?php if ($follow_up_given_by == "1"){echo "selected";} ?>>SML User</option>
                             <option value="2" <?php if ($follow_up_given_by == "2"){echo "selected";} ?>>Customer</option>
                         </select>
-                        <label for="follow_up_given_by" class="label-tag">Follow Up Given By</label>
-                        <span class='green' id='age' style="position: absolute;top: 100%;background: transparent;color: green;left: 15px;"></span> 
+                        
+                        
                     </div> 
                     <div class="form-group col-xl-2 col-lg-4 col-md-6">
                         <span class="fa-icon fa-building"></span>
