09d8d1584ec92ad0fa619830ce81af9e148c82ad
aakash <aakash@moneywide.com>
2024-02-08 16:55:48 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -359,7 +359,7 @@ $recordcount = mysqli_num_rows($res);
     <div class="gen-box white-bg">
         <div class="blue-bg col-12 font-weight-nb pb-2 pt-2 white font-20 brdr-top-gray pe-none" data-toggle="step1" id="switch_step1">
             <span id="text_step1"></span>History<div id="error-message" class="error-message"></div>
-            <div class="row div-width">
+            <!-- <div class="row div-width"> -->
             <table>
                 <tr>
                     <th>Query Id</th>
@@ -390,7 +390,7 @@ $recordcount = mysqli_num_rows($res);
                 </tr>
             </table>
 
-            </div>
+            <!-- </div> -->
 
         </div>
     </div>
