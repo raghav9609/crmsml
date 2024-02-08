9b7d97b6274133d2a02e77a35ceb429fff761312
aakash <aakash@moneywide.com>
2024-02-08 13:05:14 +0530

    as

--- insert/ajax_insert_cust_query_history.php
+++ insert/ajax_insert_cust_query_history.php
@@ -1,8 +1,7 @@
 <?php
-$slave = 1;
-require_once "../../config/config.php";
-require_once "../../include/helper.functions.php";
-require_once "../../include/display-name-functions.php";
+require_once "../config/config.php";
+require_once "../include/helper.functions.php";
+require_once "../include/display-name-functions.php";
 $type = $_REQUEST['type'];
 $return_html = "";
 /* coding for customer Query history start */
