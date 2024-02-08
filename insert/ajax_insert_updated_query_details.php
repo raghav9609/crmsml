2f6bacfa55d311b00b26232fc2a2a7ed54b80b4e
aakash <aakash@moneywide.com>
2024-02-08 10:43:11 +0530

    as

--- insert/ajax_insert_updated_query_details.php
+++ insert/ajax_insert_updated_query_details.php
@@ -13,14 +13,11 @@ $return_html = "";
 // tbl_updated_query_details.business_existing_num AS bus_exst_num, tbl_updated_query_details.annual_turnover_num AS ann_turn_num FROM tbl_mint_query INNER JOIN tbl_updated_query_details ON tbl_updated_query_details.query_id = tbl_mint_query.query_id LEFT JOIN tbl_bussiness_anl_trunover ON tbl_bussiness_anl_trunover.bus_anl_id = 
 // tbl_updated_query_details.bus_anl_trnover LEFT JOIN tbl_bussiness_extng_year ON tbl_bussiness_extng_year.bus_ext_year_id = tbl_updated_query_details.bus_ext_year WHERE tbl_mint_query.query_id = '".$qryyy_id."'");
 $qry_get_data = "select * from crm_raw_data as raw_data left join crm_query as qry on raw_data.id = qry.crm_raw_data_id where qry.id = '".$qryyy_id."'";
-echo $qry_get_data;
 $res = mysqli_query($Conn1, $qry_get_data);
 $recordcount = mysqli_num_rows($res);
-print_r($recordcount);
 if($recordcount > 0) {
     $res_data = mysqli_fetch_array($res);
-    print_r($res_data);
-    exit();
+   
     $company_nm = "";
     $cust_id = $res_data['cust_id'];
     if($res_data['company'] != '0') {
