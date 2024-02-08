3245ca287703338da69f2ab324390e5788d622f1
aakash <aakash@moneywide.com>
2024-02-08 18:26:56 +0530

    as

--- app/form_index_app.php
+++ app/form_index_app.php
@@ -171,7 +171,7 @@
                         <span class='green' id='age' style="position: absolute;top: 100%;background: transparent;color: green;left: 15px;"></span> -->
                         <span class='green' id='age' style="position: absolute;top: 100%;background: transparent;color: green;left: 15px;"></span> 
                         <label for="follow_up_given_by" class="label-tag">Follow Up Given By</label>
-                        <select id="follow_up_given_by" name="follow_up_given_by" <?php echo ($follow_up_given_by != '') ? 'readonly' : ''; ?> >
+                        <select class ="valid" id="follow_up_given_by" name="follow_up_given_by" <?php echo ($follow_up_given_by != '') ? 'readonly' : ''; ?> >
                             <option value="0">Select</option>
                             <option value="1" <?php if ($follow_up_given_by == "1"){echo "selected";} ?>>SML User</option>
                             <option value="2" <?php if ($follow_up_given_by == "2"){echo "selected";} ?>>Customer</option>
