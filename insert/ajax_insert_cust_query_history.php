bea3733904c2d7e8cae6065bfe7d659b4e3f5f44
aakash <aakash@moneywide.com>
2024-02-08 13:59:23 +0530

    as

--- insert/ajax_insert_cust_query_history.php
+++ insert/ajax_insert_cust_query_history.php
@@ -439,7 +439,7 @@ if($res_num > 0) {
 
     ?>
     <?php 
-    $return_html .= '<tr class="center-align"><td>'.$sr_no.'</td><td><span class="fs-12">(<a href="../app/edit.php?case_id='.urlencode(base64_encode($case_id)).'">'.$case_id.'</a>)</span></td><td style="width: 7%"><br><span class="fs-12">(<a href="../app/edit.php?case_id='.urlencode(base64_encode($case_id)).'">View</a>)</span></td></tr>'; 
+    $return_html .= '<tr class="center-align"><td>'.$sr_no.'</td><td><span class="fs-12">(<a href="../app/edit.php?case_id='.urlencode(base64_encode($case_id)).'">'.$case_id.'</a>)</span></td><td> </td><td style="width: 7%"><br><span class="fs-12">(<a href="../app/edit.php?case_id='.urlencode(base64_encode($case_id)).'">View</a>)</span></td></tr>'; 
     ?>
     <?php } ?>
     <?php $return_html .= '</table>';
