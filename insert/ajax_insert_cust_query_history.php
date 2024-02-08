d4349f4465b8f1c9441443b8af8a40e834c55a71
aakash <aakash@moneywide.com>
2024-02-08 13:32:33 +0530

    as

--- insert/ajax_insert_cust_query_history.php
+++ insert/ajax_insert_cust_query_history.php
@@ -194,6 +194,7 @@ if($res_num > 0) {
 
     <?php while($exe_app_history = mysqli_fetch_array($res)){
         print_r($exe_app_history);
+        echo $return_html;
     ++$sr_no;
     $f_type = $exe_app_history['follow_up_type_on'];
     $follow_up_date_on = ($exe_app_history['follow_up_date_on'] == '0000-00-00' || $exe_app_history['follow_up_date_on'] == "" || $exe_app_history['follow_up_date_on'] == "1970-01-01") ? '--' : date("d-m-Y", strtotime($exe_app_history['follow_up_date_on']));
