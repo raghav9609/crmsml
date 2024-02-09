f87936cd0e21d8089073c29ebe9e6fb7e0729e78
aakash <aakash@moneywide.com>
2024-02-09 11:02:13 +0530

    asd

--- app/update_app.php
+++ app/update_app.php
@@ -110,14 +110,16 @@ $insert_qry1 =  "INSERT INTO crm_lead_summary_history set lead_id = '".$app_id."
 $res_qry = mysqli_query($Conn1,$insert_qry1);
 
 
-if ($res_qry) {
-    $_SESSION['succ_msg'] = "Updated Sucessfully";
-} else {
-    echo "Update failed: " . mysqli_error($your_database_connection);
-}
-    echo '<script>window.location.href = "'.$head_url.'/app/edit.php?app_id='.urlencode(base64_encode($app_id)).'&cust_id='.urlencode(base64_encode($cust_id)).'&loan_type='.urlencode(base64_encode($loan_type)).'";</script>';
+// if ($res_qry) {
+//     $_SESSION['succ_msg'] = "Updated Sucessfully";
+// } else {
+//     echo "Update failed: " . mysqli_error($your_database_connection);
+// }
+$url = $head_url . '/app/edit.php?app_id=' . urlencode(base64_encode($app_id)) . '&cust_id=' . urlencode(base64_encode($cust_id)) . '&loan_type=' . urlencode(base64_encode($loan_type));
+echo "<script>window.location.href = '$url';</script>";
 exit;
 }
-echo '<script>window.location.href = "'.$head_url.'/app/edit.php?app_id='.urlencode(base64_encode($app_id)).'&cust_id='.urlencode(base64_encode($cust_id)).'&loan_type='.urlencode(base64_encode($loan_type)).'";</script>';
+$url = $head_url . '/app/edit.php?app_id=' . urlencode(base64_encode($app_id)) . '&cust_id=' . urlencode(base64_encode($cust_id)) . '&loan_type=' . urlencode(base64_encode($loan_type));
+echo "<script>window.location.href = '$url';</script>";
 exit;
 ?>
