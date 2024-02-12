665563dda55714400059c84967c2bc55c4cf3e07
aakash <aakash@moneywide.com>
2024-02-12 13:40:17 +0530

    as

--- include/login-action.php
+++ include/login-action.php
@@ -15,14 +15,9 @@ if(requestMethod() != 'POST'){
     $user_query = $get_user->userDetails(array("email_id = '".$_POST['id']."'"));
     $db_handle = new DBController();
     $user_data = $db_handle->runQuery($user_query);
-    print_r($user_data);
-    echo $_POST['otp']."-----".$user_data[0]['password'];
     if(!empty($user_data) && $user_data[0]['is_active'] == 1){
         if($user_data[0]['is_ip_restriction_enable'] == 0 || ($user_data[0]['is_ip_restriction_enable'] == 1 && ipRestrictionCheck($_SERVER['REMOTE_ADDR']) == 1)){
-            $user_password = md5($_POST['otp']);
-            echo $user_password;
-            // if(password_verify($_POST['otp'], $user_data[0]['password'])){
-            if ($user_password === $user_data[0]['password']) {
+            if(password_verify($_POST['otp'], $user_data[0]['password'])){
                 $login_history_update = $get_user->updateLoginDateTime($user_data[0]['id'],currentDateTime24());
                 $login_history_insert = $get_user->loginHistory(array('crm_master_user_id','login_ip'),array($user_data[0]['id'],$_POST['deviceId']));
                 $db_handle->insertRows($login_history_insert);
