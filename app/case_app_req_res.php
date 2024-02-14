4dfd98f7db4248694941d58edeabd40f309d3c9b
aakash <aakash@moneywide.com>
2024-02-14 17:14:21 +0530

    changes

--- app/case_app_req_res.php
+++ app/case_app_req_res.php
@@ -6,7 +6,7 @@ $query_id = $_REQUEST['query_id'];
     $api_helper_qry = mysqli_query($Conn1, "SELECT lead_id,user.name as user_name,history.created_on FROM crm_lead_summary_history as history INNER JOIN crm_master_user as user ON history.user_id = user.id  WHERE history.lead_id = '".$query_id."' and type = 1 order by history.id desc");
     if(mysqli_num_rows($api_helper_qry) > 0) {
 
-        $return_html = "<table width='' class='gridtable' id='app_rq_rs'><tbody>
+        $return_html = "<table width='100%' class='gridtable' id='app_rq_rs'><tbody>
         <tr class='font-weight-bold'><th>Sr. No.</th><th>Lead Id</th><th>User Name</th><th>Lead View Date Time</th></tr>";
         $sr_no = 0;
         while($api_helper_result  = mysqli_fetch_array($api_helper_qry)){
