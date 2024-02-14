4b24a6233f4cb557b678e0fdf3fc3eb38d579541
aakash <aakash@moneywide.com>
2024-02-14 16:43:09 +0530

    as

--- query/show_number_history.php
+++ query/show_number_history.php
@@ -1,17 +1,18 @@
 <?php 
 //$master_slave =1;
-require_once "../config/config.php";
 require_once "../config/session.php";
+require_once "../config/config.php";
+
 $id = $_REQUEST['id'];
 $src = $_REQUEST['src'];
 
 if($src == 'query'){
-    $qry_fetch = mysqli_query($Conn1,"select cust.phone as phone from crm_query as query left JOIN crm_customer as cust on query.crm_customer_id = cust.id where query.id = '".$id."'");
+    $qry_fetch = mysqli_query($Conn1,"select cust.phone_no as phone from crm_query as query left JOIN crm_customer as cust on query.crm_customer_id = cust.id where query.id = '".$id."'");
 
 } 
 
 $res_fetch = mysqli_fetch_array($qry_fetch);
 echo $phone = $res_fetch['phone'];
 
-$qry_show_num = mysqli_query($Conn_master,"Insert into crm_show_number_history set id='".$id."',user_id='".$user."',source='".$src."',datetime=NOW()");
+$qry_show_num = mysqli_query($Conn_master,"Insert into crm_show_number_history set query_id='".$id."',user_id='".$user."',datetime=NOW()");
 ?>
