429486c34428b7fdcc5c52b61602c8b732df0cf0
aakash <aakash@moneywide.com>
2024-02-14 11:59:20 +0530

    as

--- panel/password-reset/update.php
+++ panel/password-reset/update.php
@@ -20,10 +20,9 @@ if(!empty($old_password) && !empty($new_password) && !empty($confirm_new_passwor
     $enc_old_password           = md5($old_password);
     $enc_new_password           = md5($new_password);
     $enc_confirm_new_password   = md5($confirm_new_password);
-    $select_password_query = "SELECT user_id, password FROM crm_master_user WHERE id = '".$user_id."' ";
+    $select_password_query = "SELECT id, password FROM crm_master_user WHERE id = '".$user_id."' ";
     echo $select_password_query;
     $select_password_exe = mysqli_query($Conn1, $select_password_query);
-    print_r($select_password_exe);
     if(mysqli_num_rows($select_password_exe) > 0) {
         $select_password_res = mysqli_fetch_array($select_password_exe);
         echo $enc_old_password."----------".$select_password_res['password'];
