648277285367bc0a550f579946ee4fdb4b60f7da
Sumit Raghav <raghav9609@gmail.com>
2024-02-16 15:03:27 +0530

    as

--- report/query-report.php
+++ report/query-report.php
@@ -3,13 +3,14 @@ require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once(dirname(__FILE__) . '/../include/header.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
-echo $getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 GROUP by qry.query_status,qry.lead_assign_to";
+$getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 GROUP by qry.query_status,qry.lead_assign_to";
 $resreport = mysqli_query($Conn1,$getreport);
 
-$resultqryReport = mysqli_fetch_array($resreport);
+//$resultqryReport = mysqli_fetch_array($resreport);
 
 ?>
-<table width="100%" class="gridtable">
+<h2>Query Report</h2>
+<table width="80%" class="gridtable" style="margin-left:5%">
     <tbody>
         <tr>
             <th width="10%">User Name </th>
