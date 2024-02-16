3accaeb480f8d98ecd511b5fb30014be519f547e
Sumit Raghav <raghav9609@gmail.com>
2024-02-16 15:09:15 +0530

    as

--- report/query-report.php
+++ report/query-report.php
@@ -6,7 +6,7 @@ require_once(dirname(__FILE__) . '/../config/config.php');
 $getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 GROUP by qry.query_status,qry.lead_assign_to";
 $resreport = mysqli_query($Conn1,$getreport);
 
-$getappreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 2 GROUP by qry.query_status,qry.lead_assign_to";
+$getappreport = "SELECT usr.name As user_name,count(app.id) As Total_count, stat.value As status FROM `crm_query_application` As app left JOIN crm_master_user As usr ON app.user_id = usr.id LEFT JOIN crm_master_status As stat ON app.application_status = stat.id WHERE stat.status_type = 2 GROUP by app.application_status,app.user_id";
 $resappreport = mysqli_query($Conn1,$getappreport);
 ?>
 <h2>Query Report</h2>
