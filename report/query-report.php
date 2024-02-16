e87495a340834e54f41f606fe6e08474d5ab38e1
Sumit Raghav <raghav9609@gmail.com>
2024-02-16 15:34:45 +0530

    sa

--- report/query-report.php
+++ report/query-report.php
@@ -24,111 +24,209 @@ if (isset($_REQUEST['query_status'])) {
 if (isset($_REQUEST['application_status'])) {
     $application_status = replace_special($_REQUEST['application_status']);
 }
-
-$getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 ";
-
-if ($u_assign != '') {
-    $default = 1;
-        $getreport .= " and qry.lead_assign_to = '" . $u_assign . "'";
-}
-if ($query_status != '') {
-    $default = 1;
-        $getreport .= " and qry.query_status = '" . $query_status . "'";
-}
-if ($follow_date_from != '' && $follow_date_to != '') {
-    $default = 1;
-    $getreport .= " and qry.follow_date between '" . $follow_date_from . "' and '" . $follow_date_to . "' ";
-}
-if ($date_from != '' && $date_to != '') {
-    $default = 1;
-    $getreport .= " and date(qry.created_on) between '" . $date_from . "' and '" . $date_to . "' ";
-}
-$getreport .= " GROUP by qry.query_status,qry.lead_assign_to";
-
-
-echo $getreport;
-
-$resreport = mysqli_query($Conn1,$getreport);
-
-$getappreport = "SELECT usr.name As user_name,count(app.id) As Total_count, stat.value As status FROM `crm_query_application` As app left JOIN crm_master_user As usr ON app.user_id = usr.id LEFT JOIN crm_master_status As stat ON app.application_status = stat.id WHERE stat.status_type = 2 ";
-
-if ($u_assign != '') {
-    $default = 1;
-    $getappreport .= " and qry.lead_assign_to = '" . $u_assign . "'";
-}
-if ($application_status != '') {
-    $default = 1;
-    $getappreport .= " and app.application_status = '" . $application_status . "'";
-}
-if ($follow_date_from != '' && $follow_date_to != '') {
-    $default = 1;
-    $getappreport .= " and app.follow_up_date between '" . $follow_date_from . "' and '" . $follow_date_to . "' ";
-}
-if ($date_from != '' && $date_to != '') {
-    $default = 1;
-    $getappreport .= " and date(app.created_on) between '" . $date_from . "' and '" . $date_to . "' ";
-}
-$getappreport .= " GROUP by app.application_status,app.user_id";
-
-echo "<br>".$getappreport;
-$resappreport = mysqli_query($Conn1,$getappreport);
 ?>
+<!DOCTYPE html>
+    <html>
 
-<fieldset>
-    <legend>Report Filter</legend>
-    <form method="post" action="query-report.php" name="searchfrm" autocomplete="off" onsubmit="return filter_validation()">
-        <input type="text" class="text-input" name="date_from" id="date_from" placeholder="Date From" maxlength="10" value="<?php echo $date_from; ?>" readonly="readonly" />
-        <input type="text" class="text-input" name="date_to" id="date_to" placeholder="Date To" maxlength="10" value="<?php echo $date_to; ?>" readonly="readonly" />
-        <?php echo get_dropdown('query_status', 'query_statussearch', $query_statussearch, '');
-        echo get_dropdown('application_status', 'application_status', $application_status, ''); 
-        if ($user_role != 3) { 
-            echo get_dropdown('user_id_3', 'u_assign', $u_assign, ''); 
-        } ?>
-        <input type="text" class="text-input" name="follow_date_from" id="follow_date_from" placeholder="Follow Date From" maxlength="10" value="<?php echo $follow_date_from; ?>" readonly="readonly" />
-        <input type="text" class="text-input" name="follow_date_to" id="follow_date_to" placeholder="Follow Date To" maxlength="10" value="<?php echo $follow_date_to; ?>" readonly="readonly" />
-        <input class="cursor" type="submit" name="searchsubmit" value="Filter"><input class="cursor" type="button" onclick="resetform()" value="Clear">
-    </form>
-</fieldset>
+    <head>
+        <link rel="stylesheet" href="../assets/css/jquery-ui.css">
+        <script type="text/javascript" src="../assets/js/jquery-1.10.2.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery-ui.js"></script>
+        <script>
+            function selectAll(source) {
+                checkboxes = document.getElementsByName('mask[]');
+                for (var i in checkboxes)
+                    checkboxes[i].checked = source.checked;
+            }
 
-<h2>Query Report</h2>
-<table width="80%" class="gridtable" style="margin-left:5%">
-    <tbody>
-        <tr>
-            <th width="10%">User Name </th>
-            <th width="10%">Query Status</th>
-            <th width="10%">Total Leads</th>
-        </tr>
-        <?php  while($resdata = mysqli_fetch_array($resreport)){
-            if($resdata['user_name'] == ''){$userName = 'Unassigned'; } else {$userName = $resdata['user_name'];}?>
-            <tr>
-                <td><span><?php echo $userName;?> </span> </td>
-                <td><span><?php echo $resdata['status'];?> </span> </td>
-                <td><span><?php echo $resdata['Total_count'];?> </span> </td>
-            </tr>
-        <?php } ?>
-    </tbody>
-</table>
+            $(function() {
+                jQuery('#date_from').datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    dateFormat: 'yy-mm-dd',
+                    beforeShow: function() {
+                        jQuery(this).datepicker('option', 'maxDate', jQuery('#date_to').val());
+                    }
+                });
+                jQuery('#date_to').datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    dateFormat: 'yy-mm-dd',
+                    beforeShow: function() {
+                        jQuery(this).datepicker('option', 'minDate', jQuery('#date_from').val());
+                    }
+                });
+
+                jQuery('#follow_date_from').datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    dateFormat: 'yy-mm-dd',
+                    beforeShow: function() {
+                        jQuery(this).datepicker('option', 'maxDate', jQuery('#follow_date_to').val());
+                    }
+                });
+                jQuery('#follow_date_to').datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    dateFormat: 'yy-mm-dd',
+                    beforeShow: function() {
+                        jQuery(this).datepicker('option', 'minDate', jQuery('#follow_date_from').val());
+                    }
+                });
+
+                $("#assign_to").hide();
+                $("#query_search").hide();
+                $('#assign').click(function() {
+                    $('#assign_to').show();
+                    $("#query_search").hide();
+                });
+                $("#city_type").autocomplete({
+                    source: "../../include/city_name.php",
+                    minLength: 2
+                });
+                $('#query').click(function() {
+                    $('#query_search').show();
+                    $("#assign_to").hide();
+
+                });
+            });
+
+            function resetform() {
+                window.location.href = "<?php echo $head_url; ?>/query/";
+            }
+
+            function filter_validation() {
+                if ($("#email_search").val().trim() != "") {
+                    var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                    if (!email_regex.test($("#email_search").val())) {
+                        alert("Customer Email not valid")
+                        return false;
+                    }
+                }
+            }
+        </script>
+    </head>
+
+    <body>
+        <div class="color-bar-1"></div>
+        <div class="color-bar-2 color-bg"></div>
+
+        <div class="container main-container">
+            <!-- End Header -->
+
+            <div class="row">
+                <!--Container row-->
+                <!-- Title Header -->
+                <div class="span9">
+                    <?php 
+                        $getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 ";
+
+                        if ($u_assign != '') {
+                            $default = 1;
+                                $getreport .= " and qry.lead_assign_to = '" . $u_assign . "'";
+                        }
+                        if ($query_status != '') {
+                            $default = 1;
+                                $getreport .= " and qry.query_status = '" . $query_status . "'";
+                        }
+                        if ($follow_date_from != '' && $follow_date_to != '') {
+                            $default = 1;
+                            $getreport .= " and qry.follow_date between '" . $follow_date_from . "' and '" . $follow_date_to . "' ";
+                        }
+                        if ($date_from != '' && $date_to != '') {
+                            $default = 1;
+                            $getreport .= " and date(qry.created_on) between '" . $date_from . "' and '" . $date_to . "' ";
+                        }
+                        $getreport .= " GROUP by qry.query_status,qry.lead_assign_to";
+                        
+                        
+                        echo $getreport;
+
+                        $resreport = mysqli_query($Conn1,$getreport);
+
+                        $getappreport = "SELECT usr.name As user_name,count(app.id) As Total_count, stat.value As status FROM `crm_query_application` As app left JOIN crm_master_user As usr ON app.user_id = usr.id LEFT JOIN crm_master_status As stat ON app.application_status = stat.id WHERE stat.status_type = 2 ";
+
+                        if ($u_assign != '') {
+                            $default = 1;
+                            $getappreport .= " and qry.lead_assign_to = '" . $u_assign . "'";
+                        }
+                        if ($application_status != '') {
+                            $default = 1;
+                            $getappreport .= " and app.application_status = '" . $application_status . "'";
+                        }
+                        if ($follow_date_from != '' && $follow_date_to != '') {
+                            $default = 1;
+                            $getappreport .= " and app.follow_up_date between '" . $follow_date_from . "' and '" . $follow_date_to . "' ";
+                        }
+                        if ($date_from != '' && $date_to != '') {
+                            $default = 1;
+                            $getappreport .= " and date(app.created_on) between '" . $date_from . "' and '" . $date_to . "' ";
+                        }
+                        $getappreport .= " GROUP by app.application_status,app.user_id";
+
+                        echo "<br>".$getappreport;
+                        $resappreport = mysqli_query($Conn1,$getappreport);
+                    ?>
+
+                    <fieldset>
+                        <legend>Report Filter</legend>
+                        <form method="post" action="query-report.php" name="searchfrm" autocomplete="off" onsubmit="return filter_validation()">
+                            <input type="text" class="text-input" name="date_from" id="date_from" placeholder="Date From" maxlength="10" value="<?php echo $date_from; ?>" readonly="readonly" />
+                            <input type="text" class="text-input" name="date_to" id="date_to" placeholder="Date To" maxlength="10" value="<?php echo $date_to; ?>" readonly="readonly" />
+                            <?php echo get_dropdown('query_status', 'query_statussearch', $query_statussearch, '');
+                            echo get_dropdown('application_status', 'application_status', $application_status, ''); 
+                            if ($user_role != 3) { 
+                                echo get_dropdown('user_id_3', 'u_assign', $u_assign, ''); 
+                            } ?>
+                            <input type="text" class="text-input" name="follow_date_from" id="follow_date_from" placeholder="Follow Date From" maxlength="10" value="<?php echo $follow_date_from; ?>" readonly="readonly" />
+                            <input type="text" class="text-input" name="follow_date_to" id="follow_date_to" placeholder="Follow Date To" maxlength="10" value="<?php echo $follow_date_to; ?>" readonly="readonly" />
+                            <input class="cursor" type="submit" name="searchsubmit" value="Filter"><input class="cursor" type="button" onclick="resetform()" value="Clear">
+                        </form>
+                    </fieldset>
+
+                    <h2>Query Report</h2>
+                    <table width="80%" class="gridtable" style="margin-left:5%">
+                        <tbody>
+                            <tr>
+                                <th width="10%">User Name </th>
+                                <th width="10%">Query Status</th>
+                                <th width="10%">Total Leads</th>
+                            </tr>
+                        <?php  while($resdata = mysqli_fetch_array($resreport)){
+                            if($resdata['user_name'] == ''){$userName = 'Unassigned'; } else {$userName = $resdata['user_name'];}?>
+                            <tr>
+                                <td><span><?php echo $userName;?> </span> </td>
+                                <td><span><?php echo $resdata['status'];?> </span> </td>
+                                <td><span><?php echo $resdata['Total_count'];?> </span> </td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
 
 
-<h2>Application Report</h2>
-<table width="80%" class="gridtable" style="margin-left:5%">
-    <tbody>
-        <tr>
-            <th width="10%">User Name </th>
-            <th width="10%">Query Status</th>
-            <th width="10%">Total Leads</th>
-        </tr>
-        <?php  while($resappdata = mysqli_fetch_array($resappreport)){
-            if($resappdata['user_name'] == ''){$userappName = 'Unassigned'; } else {$userappName = $resappdata['user_name'];}?>
-            <tr>
-                <td><span><?php echo $userappName;?> </span> </td>
-                <td><span><?php echo $resappdata['status'];?> </span> </td>
-                <td><span><?php echo $resappdata['Total_count'];?> </span> </td>
-            </tr>
-        <?php } ?>
-    </tbody>
-</table>
-
+                    <h2>Application Report</h2>
+                    <table width="80%" class="gridtable" style="margin-left:5%">
+                        <tbody>
+                            <tr>
+                                <th width="10%">User Name </th>
+                                <th width="10%">Query Status</th>
+                                <th width="10%">Total Leads</th>
+                            </tr>
+                            <?php  while($resappdata = mysqli_fetch_array($resappreport)){
+                                if($resappdata['user_name'] == ''){$userappName = 'Unassigned'; } else {$userappName = $resappdata['user_name'];}?>
+                                <tr>
+                                    <td><span><?php echo $userappName;?> </span> </td>
+                                    <td><span><?php echo $resappdata['status'];?> </span> </td>
+                                    <td><span><?php echo $resappdata['Total_count'];?> </span> </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
 <?php //print_r($resultqryReport);
 
 ?>
