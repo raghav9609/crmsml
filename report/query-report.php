b43b766847bc0bdc4d51b2aed77ed9a1c28ffe0c
Sumit Raghav <raghav9609@gmail.com>
2024-02-16 15:06:52 +0530

    as

--- report/query-report.php
+++ report/query-report.php
@@ -6,8 +6,8 @@ require_once(dirname(__FILE__) . '/../config/config.php');
 $getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 GROUP by qry.query_status,qry.lead_assign_to";
 $resreport = mysqli_query($Conn1,$getreport);
 
-//$resultqryReport = mysqli_fetch_array($resreport);
-
+$getappreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 2 GROUP by qry.query_status,qry.lead_assign_to";
+$resappreport = mysqli_query($Conn1,$getappreport);
 ?>
 <h2>Query Report</h2>
 <table width="80%" class="gridtable" style="margin-left:5%">
@@ -23,6 +23,26 @@ $resreport = mysqli_query($Conn1,$getreport);
                 <td><span><?php echo $userName;?> </span> </td>
                 <td><span><?php echo $resdata['status'];?> </span> </td>
                 <td><span><?php echo $resdata['Total_count'];?> </span> </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+
+<h2>Application Report</h2>
+<table width="80%" class="gridtable" style="margin-left:5%">
+    <tbody>
+        <tr>
+            <th width="10%">User Name </th>
+            <th width="10%">Query Status</th>
+            <th width="10%">Total Leads</th>
+        </tr>
+        <?php  while($resappdata = mysqli_fetch_array($resappreport)){
+            if($resappdata['user_name'] == ''){$userappName = 'Unassigned'; } else {$userappName = $resappdata['user_name'];}?>
+            <tr>
+                <td><span><?php echo $userappName;?> </span> </td>
+                <td><span><?php echo $resappdata['status'];?> </span> </td>
+                <td><span><?php echo $resappdata['Total_count'];?> </span> </td>
             </tr>
         <?php } ?>
     </tbody>
