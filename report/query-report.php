9bad4c6f4a1af894637c07e15640427e8522a398
Sumit Raghav <raghav9609@gmail.com>
2024-02-16 14:39:43 +0530

    as

--- report/query-report.php
+++ report/query-report.php
@@ -3,7 +3,6 @@ require_once(dirname(__FILE__) . '/../config/session.php');
 require_once(dirname(__FILE__) . '/../helpers/common-helper.php');
 require_once(dirname(__FILE__) . '/../include/header.php');
 require_once(dirname(__FILE__) . '/../config/config.php');
-
 $getreport = "SELECT usr.name As user_name,count(qry.id) As Total_count, stat.value As status FROM `crm_query` As qry left JOIN crm_master_user As usr ON qry.lead_assign_to = usr.id LEFT JOIN crm_master_status As stat ON qry.query_status = stat.id WHERE stat.status_type = 1 GROUP by qry.query_status,qry.lead_assign_to";
 $resreport = mysqli_query($Conn1,$getreport);
 
