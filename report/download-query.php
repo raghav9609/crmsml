06ef0f3c6c17f1264d7a7c228ac1e54315d506ff
Sumit Raghav <raghav9609@gmail.com>
2024-02-17 14:59:56 +0530

    as

--- report/download-query.php
+++ report/download-query.php
@@ -43,7 +43,7 @@ while($rs = mysqli_fetch_array($results)) {
         }
         $city_name = 'Other';
         if($rs["city_id"] > 0 ){
-            $city_nameval = get_name('city_id',$res["city_id"]);
+            $city_nameval = get_name('city_id',$rs["city_id"]);
             $city_name = $city_nameval["city_name"];
         }
         $toolType = 'Direct';
