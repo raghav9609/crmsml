b0a9f38b733fcf5db50ff252562fca3e253a50ae
Sumit Raghav <raghav9609@gmail.com>
2024-02-17 16:57:36 +0530

    sa

--- app/bank_type_id_fetch.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('../../include/config.php');
-$bank_id = mysqli_query($Conn1,"select bank_type_id from tbl_bank where bank_id = '".$_REQUEST['bank_id']."'");
-$res_bank_id = mysqli_fetch_array($bank_id);
-echo $bank_type_id = $res_bank_id['bank_type_id'];
-mysqli_close($Conn1);
-?>
