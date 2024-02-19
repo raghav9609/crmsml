d91594b18a85104475c47e6526348afd4b859f56
Sumit Raghav <raghav9609@gmail.com>
2024-02-19 10:58:55 +0530

    as

--- README.md
+++
@@ -1,2 +0,0 @@
-# crmsml
-smlCRM
--- model/authModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Auth{
-    function apiAuth($source,$api_key,$api_client){
-        return "select * from api_credentials where api_name = '".base64_encode(base64_encode($api_client))."' and api_client ='".$source."' and api_key = '".md5($api_key)."'";
-    }
-}
-?>
