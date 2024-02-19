766e3d03a2ccbe3c955b394e4d558bf92495def4
Sumit Raghav <raghav9609@gmail.com>
2024-02-19 18:14:58 +0530

    as

--- insert/ajax_insert_cust_query_history.php
+++ insert/ajax_insert_cust_query_history.php
@@ -6,37 +6,7 @@ require_once "../include/display-name-functions.php";
 $type = $_REQUEST['type'];
 $return_html = "";
 /* coding for customer Query history start */
-if($type == "query") {
-    $cust_id = $_REQUEST['cust_id'];
-    $query_history_query = "select stats.query_status,qry.query_id as id,qry.loan_amt as loan_amt,loan.loan_type_name as loan_type_name,stats.date as date,user.user_name as user_name from tbl_mint_query as qry left JOIN tbl_mint_query_status_detail as stats ON qry.query_id = stats.query_id left join lms_loan_type as loan on qry.loan_type = loan.loan_type_id left join tbl_user_assign as user on stats.user_id = user.user_id where qry.cust_id = ".$cust_id." order by qry.query_id desc limit 10";
-    $query_history_result= mysqli_query($Conn1,$query_history_query);
-    $query_history = mysqli_num_rows($query_history_result);
-    $sr_no = 0;
-    if($query_history > 0) {
-        $return_html .= "<table  class='gridtable' width='100%'><tr><th>Sr. No.</th><th>Query Id</th><th>Loan Type & Amount </th><th>Status & User</th><th>Date</th></tr>";
-        while($query_history_info = mysqli_fetch_array($query_history_result)) {
-            ++$sr_no;
-            $id = $query_history_info['id'];
-            $loan_amt = $query_history_info['loan_amt'];
-            $date = $query_history_info['date'];
-            $loan_name = $query_history_info['loan_type_name'];
-            $user_name = $query_history_info['user_name'];
-            $query_status = $query_history_info['query_status'];
-            $query_status_name = get_display_name('query_status',$query_status);
-            if($query_status_name == ''){
-                $query_status_name = get_display_name('new_status_name',$query_status);
-            }
-            $enc_id = urlencode(base64_encode($query_history_info['id']));
-            $user_by = (trim($query_history_info['user_name']) != "") ? "(By: ".$query_history_info['user_name'].")" : "";
-            $loan_amount = (($query_history_info['loan_amt'] > 0) && is_numeric($query_history_info['loan_amt'])) ? "(".custom_money_format($query_history_info['loan_amt']).")" : "";
-            $return_html .= "<tr class='center-align'><td>".$sr_no."</td><td><a href='../query/edit.php?id=$enc_id&ut=2' target ='_blank'>".$query_history_info['id']."</a></td><td>".$query_history_info['loan_type_name']."<br><span class='fs-12'>".$loan_amount."</span></td><td>".$query_history_info['qy_status']."<br><span class='fs-12'>".$user_by."</span></td><td>".date('d-m-Y',strtotime($query_history_info['date']))."</td></tr>";
-        }
-        $return_html .= "</table><br>";
-    /*}*/
-    } else {
-        $return_html = "";
-    }
-}
+
 
 if($type == "app") {
    
