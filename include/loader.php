144951930f49aa43af9ff8ae7b293b22ae8af389
Sumit Raghav <raghav9609@gmail.com>
2024-02-19 14:23:09 +0530

    as

--- include/loader.php
+++
@@ -1,8 +0,0 @@
-<div id="loader" class="lds-css"> 
-    <div class="lds-spinner">
-        <img src="<?php echo $head_url; ?>/assets/images/loader-image.gif" alt="Loader" title="Loader">
-    </div>
-    <b class="loader_text" style="letter-spacing:0.5px" id="loader_txt">
-        Please wait......
-    </b>                
-</div> 
