6d6a5681c5655cbb65456ba64012773349030675
Sumit Raghav <raghav9609@gmail.com>
2024-02-19 11:00:42 +0530

    as

--- app/edit.php
+++ app/edit.php
@@ -254,18 +254,19 @@ var minimum_days = 0;
         autoclose: true,
     });
     $('#follow_up_time').timepicker({disableTextInput: true}).val(""); 
-    // $('#follow_up_time').timepicker('option', {
-    //     minTime: '09:30:00', 
-    //     maxTime: '20:00:00', 
-    //     step: int1,
-    //     disableTextInput: true ,
-    //     disableTimeRanges: disbleslot     });
+    var int1=15;
     $('#follow_up_time').timepicker('option', {
-              minTime: '09:30:00', 
-              maxTime: '20:00:00',
-              step: 30,
-              disableTextInput: true
-          });
+        minTime: '09:30:00', 
+        maxTime: '20:00:00', 
+        step: int1,
+        disableTextInput: true ,
+        disableTimeRanges: disbleslot     });
+    // $('#follow_up_time').timepicker('option', {
+    //           minTime: '09:30:00', 
+    //           maxTime: '20:00:00',
+    //           step: 30,
+    //           disableTextInput: true
+    //       });
 </script>
 <?php } ?>
 
