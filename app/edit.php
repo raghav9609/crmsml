c804ba4cbc2b72299cbf9e1bd3c69474b8e5c9c0
Sumit Raghav <raghav9609@gmail.com>
2024-02-19 11:36:54 +0530

    as

--- app/edit.php
+++ app/edit.php
@@ -269,7 +269,6 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
               if(datetoday == date) {
                   $('#follow_up_time').timepicker({disableTextInput: true}).val("");
                   var min = formatTime(today);
-                  alert(min);
                   if(min) {
                       var minval = min;
                   } else {
@@ -298,7 +297,6 @@ if ($exe_form['id'] == '' || $exe_form['id'] == 0) {
 
       function formatTime(dt) {
         var hour = dt.getHours();
-        alert(hour);
         var hournext = dt.getHours()+1;
         var chhour = ((dt.getMinutes() >= 30) ? hournext : hour);
         if(18 > chhour) {
